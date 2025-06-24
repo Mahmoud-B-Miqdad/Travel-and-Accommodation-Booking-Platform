e58f38d325b208522a34a8ce43fab87251a9923f
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-24 09:47:51 +0300

    feat(domain): Add City aggregate with basic properties.

---
+++ TravelEase.Domain/Aggregates/City/City.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelEase.Domain.Aggregates.City
+{
+    public class City
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
+        public string PostOffice { get; set; }
+    }
+}
