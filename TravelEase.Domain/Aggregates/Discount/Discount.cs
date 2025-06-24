13885c07029c71489e4393074e575f3a64a11be0
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-24 10:41:03 +0300

    feat(domain): Add Discount aggregate with basic properties.

---
+++ TravelEase.Domain/Aggregates/Discount/Discount.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelEase.Domain.Aggregates.Discount
+{
+    public class Discount
+    {
+        public Guid Id { get; set; }
+        public float DiscountPercentage { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+}
