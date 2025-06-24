499d42a5a56fe735dbe6c7ed532bc5515c36f514
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-24 09:50:03 +0300

    feat(domain): Add Booking aggregate with basic properties.

---
+++ TravelEase.Domain/Aggregates/Booking/Booking.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelEase.Domain.Aggregates.Booking
+{
+    public class Booking
+    {
+        public Guid Id { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public DateTime BookingDate { get; set; }
+        public double Price { get; set; }
+    }
+}
