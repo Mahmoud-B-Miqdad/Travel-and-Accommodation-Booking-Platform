8580c5703aec5af67c4cf804be7a3e239f28be59
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-24 09:30:01 +0300

    feat(domain): Add Hotel aggregate with basic properties.

---
+++ TravelEase.Domain/Aggregates/Hotle/Hotle.cs
@@ -0,0 +1,15 @@
+﻿namespace TravelEase.Domain.Aggregates.Hotle
+{
+    public class Hotle
+    {
+        public Guid Id { get; set; }
+        public Guid CityId { get; set; }
+        public string Name { get; set; }
+        public float Rating { get; set; }
+        public string StreetAddress { get; set; }
+        public string Description { get; set; }
+        public string PhoneNumber { get; set; }
+        public int FloorsNumber { get; set; }
+        public string OnwerName { get; set; }
+    }
+}
