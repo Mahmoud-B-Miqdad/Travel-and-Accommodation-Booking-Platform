9b7d5a026ab5df6f851bced1d2df592a2bda23cb
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-24 13:13:19 +0300

    feat(infrastructure): Add TravelEaseDbContext with DbSets and EF Core setup.

---
+++ TravelEase.Infrastructure/Persistence/TravelEaseDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelEase.Domain.Aggregates.Bookings;
+using TravelEase.Domain.Aggregates.Cities;
+using TravelEase.Domain.Aggregates.Discounts;
+using TravelEase.Domain.Aggregates.Hotels;
+using TravelEase.Domain.Aggregates.Reviews;
+using TravelEase.Domain.Aggregates.RoomAmenities;
+using TravelEase.Domain.Aggregates.Rooms;
+using TravelEase.Domain.Aggregates.RoomTypes;
+using TravelEase.Domain.Aggregates.Users;
+
+namespace TravelEase.Infrastructure.Persistence
+{
+    public class TravelEaseDbContext : DbContext
+    {
+        public TravelEaseDbContext(DbContextOptions<TravelEaseDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<RoomAmenity> RoomAmenities { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+    }
+}
