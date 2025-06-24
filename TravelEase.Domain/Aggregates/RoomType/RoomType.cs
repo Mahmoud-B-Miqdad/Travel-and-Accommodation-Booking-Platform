3a17adf6483f8801220de307759f86681d1f27b8
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-24 10:55:43 +0300

    feat(domain): Add RoomType aggregate with pricing and RoomCategory enum.

---
+++ TravelEase.Domain/Aggregates/RoomType/RoomType.cs
@@ -0,0 +1,12 @@
+﻿using TravelEase.Domain.Enums;
+
+namespace TravelEase.Domain.Aggregates.RoomType
+{
+    public class RoomType
+    {
+        public Guid Id { get; set; }
+        public Guid HotelId { get; set; }
+        public RoomCategory Category { get; set; }
+        public float PricePerNight { get; set; }
+    }
+}
