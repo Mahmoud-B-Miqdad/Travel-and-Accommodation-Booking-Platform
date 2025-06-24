ed23691cc677bad60caf758fea5a9b05b6476bed
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-24 10:46:07 +0300

    feat(domain): Add RoomAmenity aggregate with basic properties.

---
+++ TravelEase.Domain/Aggregates/RoomAmenity/RoomAmenity.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelEase.Domain.Aggregates.RoomAmenity
+{
+    public class RoomAmenity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
