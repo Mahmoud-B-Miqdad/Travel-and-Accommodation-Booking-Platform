fdb83c3b53ce0b3d7f2d9476e5fc9d513105099a
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-24 10:20:03 +0300

    feat(domain): Add Review aggregate with basic properties.

---
+++ TravelEase.Domain/Aggregates/Review/Review.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelEase.Domain.Aggregates.Review
+{
+    public class Review
+    {
+        public Guid Id { get; set; }
+        public string Comment { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public float Rating { get; set; }
+    }
+}
