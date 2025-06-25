54ac4a4216e086b30cb451d091d32eb7cc1e6389
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-25 14:29:18 +0300

    feat(domain/infrastructure): Add IRoomRepository interface and implement RoomRepository with paginated hotel Rooms query.

---
+++ TravelEase.Domain/Aggregates/Rooms/IRoomRepository.cs
@@ -0,0 +1,10 @@
+﻿using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Domain.Common.Models.PaginationModels;
+
+namespace TravelEase.Domain.Aggregates.Rooms
+{
+    public interface IRoomRepository : ICrudRepository<Room>
+    {
+        Task<PaginatedList<Room>> GetAllByHotelIdAsync(Guid hotelId, string? searchQuery, int pageNumber, int pageSize);
+    }
+}
---
+++ TravelEase.Infrastructure/Persistence/EntityPersistence/RoomPersistence/RoomRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelEase.Domain.Aggregates.Rooms;
+using TravelEase.Domain.Common.Models.PaginationModels;
+using TravelEase.Infrastructure.Common.Helpers;
+using TravelEase.Infrastructure.Persistence.Repositories;
+
+namespace TravelEase.Infrastructure.Persistence.EntityPersistence.RoomPersistence
+{
+    public class RoomRepository : GenericCrudRepository<Room>, IRoomRepository
+    {
+        private readonly TravelEaseDbContext _context;
+
+        public RoomRepository(TravelEaseDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<PaginatedList<Room>> GetAllByHotelIdAsync(Guid hotelId, string? searchQuery, int pageNumber, int pageSize)
+        {
+            var query = from room in _context.Rooms
+                        join roomType in _context.RoomTypes on room.RoomTypeId equals roomType.Id
+                        where roomType.HotelId == hotelId
+                        select room;
+
+            if (!string.IsNullOrWhiteSpace(searchQuery))
+            {
+                searchQuery = searchQuery.Trim();
+                query = query.Where(room => room.View.Contains(searchQuery));
+            }
+
+            return await PaginationHelper.PaginateAsync(query.AsNoTracking(), pageNumber, pageSize);
+        }
+    }
+}
