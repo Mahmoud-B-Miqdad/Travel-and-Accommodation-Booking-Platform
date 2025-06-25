e37eb3ca6b2f68e210b509b63a354d200ae96992
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-25 13:46:33 +0300

    feat(infrastructure): Add GenericCrudRepository with basic CRUD operations implementation.

---
+++ TravelEase.Infrastructure/Persistence/Repositories/GenericCrudRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelEase.Domain.Common.Interfaces;
+
+namespace TravelEase.Infrastructure.Persistence.Repositories
+{
+    public class GenericCrudRepository<T> : ICrudRepository<T> where T : class
+    {
+        protected readonly DbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericCrudRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public virtual async Task<T?> GetByIdAsync(Guid id) => await _dbSet.FindAsync(id);
+
+        public virtual async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+
+        public virtual void Update(T entity) => _dbSet.Update(entity);
+
+        public virtual void Remove(T entity) => _dbSet.Remove(entity);
+    }
+}
