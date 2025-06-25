ffe1f84b04c9979581094d5d48922da1cb84347e
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-25 15:41:54 +0300

    feat(domain/infrastructure): Add IHotelRepository and implement HotelRepository with search filtering and pagination.

---
+++ TravelEase.Domain/Aggregates/Hotels/IHotelRepository.cs
@@ -0,0 +1,10 @@
+﻿using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Domain.Common.Models.PaginationModels;
+
+namespace TravelEase.Domain.Aggregates.Hotels
+{
+    public interface IHotelRepository : ICrudRepository<Hotel>
+    {
+        Task<PaginatedList<Hotel>> GetAllAsync(string? searchQuery, int pageNumber, int pageSize);
+    }
+}
---
+++ TravelEase.Infrastructure/Persistence/EntityPersistence/HotelPersistence/HotelRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelEase.Domain.Aggregates.Hotels;
+using TravelEase.Domain.Common.Models.PaginationModels;
+using TravelEase.Infrastructure.Common.Helpers;
+using TravelEase.Infrastructure.Persistence.Repositories;
+
+namespace TravelEase.Infrastructure.Persistence.EntityPersistence.HotelPersistence
+{
+    public class HotelRepository : GenericCrudRepository<Hotel>, IHotelRepository
+    {
+        private readonly TravelEaseDbContext _context;
+
+        public HotelRepository(TravelEaseDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<PaginatedList<Hotel>> GetAllAsync(string? searchQuery, int pageNumber, int pageSize)
+        {
+            var query = _context.Hotels.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(searchQuery))
+            {
+                searchQuery = searchQuery.Trim();
+                query = query.Where(hotel =>
+                    hotel.Name.Contains(searchQuery) ||
+                    hotel.Description.Contains(searchQuery) ||
+                    hotel.StreetAddress.Contains(searchQuery));
+            }
+
+            return await PaginationHelper.PaginateAsync(query.AsNoTracking(), pageNumber, pageSize);
+        }
+    }
+}
