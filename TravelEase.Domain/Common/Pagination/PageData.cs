a8a3a21f4d44e89c04501414c75b623c7000662c
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-25 13:15:50 +0300

    feat(domain): Add PageData model to support pagination metadata.

---
+++ TravelEase.Domain/Common/Pagination/PageData.cs
@@ -0,0 +1,17 @@
+﻿namespace TravelEase.Domain.Common.Pagination
+{
+    public class PageData
+    {
+        public int TotalItems { get; }
+        public int PageSize { get; }
+        public int CurrentPage { get; }
+        public int TotalPages => (int)Math.Ceiling(TotalItems / (double)PageSize);
+
+        public PageData(int totalItems, int pageSize, int currentPage)
+        {
+            TotalItems = totalItems;
+            PageSize = pageSize;
+            CurrentPage = currentPage;
+        }
+    }
+}
