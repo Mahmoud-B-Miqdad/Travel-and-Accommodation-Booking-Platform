5adfd98344809eb46509fe1e25a3ff01bcd0921e
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-25 14:38:14 +0300

    feat(domain/infrastructure): Add IDiscountRepository interface and implement DiscountRepository with paginated RoomType Discounts query.

---
+++ TravelEase.Domain/Aggregates/Discounts/IDiscountRepository.cs
@@ -0,0 +1,11 @@
+﻿using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Domain.Common.Models.PaginationModels;
+
+namespace TravelEase.Domain.Aggregates.Discounts
+{
+    public interface IDiscountRepository : ICrudRepository<Discount>
+    {
+        Task<PaginatedList<Discount>> GetAllByRoomTypeIdAsync(Guid roomTypeId, int pageNumber, int pageSize);
+    }
+
+}
---
+++ TravelEase.Infrastructure/Persistence/EntityPersistence/DiscountPersistence/DiscountRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using TravelEase.Domain.Aggregates.Discounts;
+using TravelEase.Domain.Common.Models.PaginationModels;
+using TravelEase.Infrastructure.Common.Helpers;
+using TravelEase.Infrastructure.Persistence.Repositories;
+
+namespace TravelEase.Infrastructure.Persistence.EntityPersistence.DiscountPersistence
+{
+    public class DiscountRepository : GenericCrudRepository<Discount>, IDiscountRepository
+    {
+        private readonly TravelEaseDbContext _context;
+
+        public DiscountRepository(TravelEaseDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<PaginatedList<Discount>> GetAllByRoomTypeIdAsync(Guid roomTypeId, int pageNumber, int pageSize)
+        {
+            var query = _context.Discounts
+                .Where(discount => discount.RoomTypeId == roomTypeId);
+
+            return await PaginationHelper.PaginateAsync(query.AsNoTracking(), pageNumber, pageSize);
+        }
+    }
+}
