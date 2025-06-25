c40d08254fb1e8134ccfc0b04c0941e9bd13c36a
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-25 13:19:06 +0300

    feat(domain): Add IReadableRepository interface with paginated GetAllAsync method.

---
+++ TravelEase.Domain/Common/Interfaces/IReadableRepository.cs
@@ -0,0 +1,9 @@
+﻿using TravelEase.Domain.Common.Pagination;
+
+namespace TravelEase.Domain.Common.Interfaces
+{
+    public interface IReadableRepository<T> where T : class
+    {
+        Task<PaginatedList<T>> GetAllAsync(int pageNumber, int pageSize);
+    }
+}
