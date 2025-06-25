f219c32b283d43a9af18bfdf5d461049b383ba92
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-25 13:25:45 +0300

    feat(infrastructure): Add PaginationHelper to support asynchronous pagination for IQueryable sources.

---
+++ TravelEase.Infrastructure/Helpers/PaginationHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using TravelEase.Domain.Common.Pagination;
+
+namespace TravelEase.Infrastructure.Helpers
+{
+    public static class PaginationHelper
+    {
+        public static async Task<PaginatedList<T>> PaginateAsync<T>(
+            IQueryable<T> query,
+            int pageNumber,
+            int pageSize) where T : class
+        {
+            var totalItemCount = await query.CountAsync();
+            var pageData = new PageData(totalItemCount, pageSize, pageNumber);
+
+            var result = await query
+                .Skip(pageSize * (pageNumber - 1))
+                .Take(pageSize)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return new PaginatedList<T>(result, pageData);
+        }
+    }
+}
