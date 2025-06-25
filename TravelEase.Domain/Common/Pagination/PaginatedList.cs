308de380bfbfd86e9d7ed0e80ed596890421adc0
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-25 13:18:02 +0300

    feat(domain): Add generic PaginatedList to encapsulate items with pagination metadata.

---
+++ TravelEase.Domain/Common/Pagination/PaginatedList.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelEase.Domain.Common.Pagination
+{
+    public class PaginatedList<T>
+    {
+        public List<T> Items { get; }
+        public PageData PageData { get; }
+
+        public PaginatedList(List<T> items, PageData pageData)
+        {
+            Items = items;
+            PageData = pageData;
+        }
+    }
+}
