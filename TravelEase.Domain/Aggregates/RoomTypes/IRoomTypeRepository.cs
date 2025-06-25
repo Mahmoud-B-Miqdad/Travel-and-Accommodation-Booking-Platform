62c73f848d62569e8534d2ddb81f8bb0b44b9c5d
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-25 14:49:35 +0300

    feat(domain/infrastructure): Add IRoomTypeRepository interface and implement RoomTypeRepository with paginated hotel RoomTypes query

---
+++ TravelEase.Domain/Aggregates/RoomTypes/IRoomTypeRepository.cs
@@ -0,0 +1,10 @@
+﻿using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Domain.Common.Models.PaginationModels;
+
+namespace TravelEase.Domain.Aggregates.RoomTypes
+{
+    public interface IRoomTypeRepository : ICrudRepository<RoomType>
+    {
+        Task<PaginatedList<RoomType>> GetAllByHotelIdAsync(Guid hotelId, bool includeAmenities, int pageNumber, int pageSize);
+    }
+}
---
+++ TravelEase.Infrastructure/Persistence/EntityPersistence/RoomTypePersistence/RoomTypeRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using TravelEase.Domain.Aggregates.RoomTypes;
+using TravelEase.Domain.Common.Models.PaginationModels;
+using TravelEase.Infrastructure.Common.Helpers;
+using TravelEase.Infrastructure.Persistence.Repositories;
+
+namespace TravelEase.Infrastructure.Persistence.EntityPersistence.RoomTypePersistence
+{
+    public class RoomTypeRepository : GenericCrudRepository<RoomType>, IRoomTypeRepository
+    {
+        private readonly TravelEaseDbContext _context;
+
+        public RoomTypeRepository(TravelEaseDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<PaginatedList<RoomType>> GetAllByHotelIdAsync(Guid hotelId, bool includeAmenities, int pageNumber, int pageSize)
+        {
+            var query = _context.RoomTypes
+                .Where(rt => rt.HotelId == hotelId)
+                .AsQueryable();
+
+            if (includeAmenities)
+            {
+                query = query.Include(rt => rt.Amenities);
+            }
+
+            return await PaginationHelper.PaginateAsync(query.AsNoTracking(), pageNumber, pageSize);
+        }
+    }
+}
