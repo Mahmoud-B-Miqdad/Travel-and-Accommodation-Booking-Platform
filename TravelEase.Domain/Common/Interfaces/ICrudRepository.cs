1a13ca9aae5865d830af479c79429ed9f63f46e4
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-25 13:09:03 +0300

    feat(domain): Add generic ICrudRepository interface for basic data operations.

---
+++ TravelEase.Domain/Common/Interfaces/ICrudRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelEase.Domain.Common.Interfaces
+{
+    public interface ICrudRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(Guid id);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+    }
+}
