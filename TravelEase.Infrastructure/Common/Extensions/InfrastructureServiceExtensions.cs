d474edc2304dc9d8de7dc64abb6ee5f5bde34f5b
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-26 16:36:59 +0300

    chore(infrastructure): Add dependency injection extension to register repositories and UnitOfWork services.

---
+++ TravelEase.Infrastructure/Common/Extensions/InfrastructureServiceExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TravelEase.Domain.Aggregates.Bookings;
+using TravelEase.Domain.Aggregates.Cities;
+using TravelEase.Domain.Aggregates.Discounts;
+using TravelEase.Domain.Aggregates.Hotels;
+using TravelEase.Domain.Aggregates.Reviews;
+using TravelEase.Domain.Aggregates.RoomAmenities;
+using TravelEase.Domain.Aggregates.Rooms;
+using TravelEase.Domain.Aggregates.RoomTypes;
+using TravelEase.Domain.Aggregates.Users;
+using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Infrastructure.Persistence.Context;
+using TravelEase.Infrastructure.Persistence.EntityPersistence.BookingPersistence;
+using TravelEase.Infrastructure.Persistence.EntityPersistence.CityPersistence;
+using TravelEase.Infrastructure.Persistence.EntityPersistence.DiscountPersistence;
+using TravelEase.Infrastructure.Persistence.EntityPersistence.HotelPersistence;
+using TravelEase.Infrastructure.Persistence.EntityPersistence.ReviewPersistence;
+using TravelEase.Infrastructure.Persistence.EntityPersistence.RoomAmenityPersistence;
+using TravelEase.Infrastructure.Persistence.EntityPersistence.RoomPersistence;
+using TravelEase.Infrastructure.Persistence.EntityPersistence.RoomTypePersistence;
+using TravelEase.Infrastructure.Persistence.EntityPersistence.UserPersistence;
+
+namespace TravelEase.Infrastructure.Common.Extensions
+{
+    public static class InfrastructureServiceExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<ICityRepository, CityRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IRoomAmenityRepository, RoomAmenityRepository>();
+            services.AddScoped<IReviewRepository, ReviewRepository>();
+            services.AddScoped<IBookingRepository, BookingRepository>();
+            services.AddScoped<IRoomRepository, RoomRepository>();
+            services.AddScoped<IHotelRepository, HotelRepository>();
+            services.AddScoped<IRoomTypeRepository, RoomTypeRepository>();
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+    }
+}
