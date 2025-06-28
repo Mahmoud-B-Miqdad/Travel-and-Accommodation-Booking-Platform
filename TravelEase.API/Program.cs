787ba8173618c9c13ee3636d0e25ff08b4a8d27d
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-28 16:14:00 +0300

    chore(api): Configure service registrations for Application layer, FluentValidation, content negotiation, and Swagger with XML comments support.

--- TravelEase.API/Program.cs
+++ TravelEase.API/Program.cs
@@ -1,17 +1,42 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 using TravelEase.API.Validators.CityValidators;
 using TravelEase.Infrastructure.Common.Extensions;
+using TravelEase.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = false;
+}).AddNewtonsoftJson()
+  .AddXmlDataContractSerializerFormatters();
+
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddValidatorsFromAssemblyContaining<GetAllCitiesQueryValidator>();
 
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(setupAction =>
+{
+    var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+    setupAction.IncludeXmlComments(xmlCommentsFullPath);
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
