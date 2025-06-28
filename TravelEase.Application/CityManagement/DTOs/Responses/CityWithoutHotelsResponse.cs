e877a8c50a5ed46accb83c928fe3df466f76fc21
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-28 12:26:08 +0300

    feat(application): Add CityWithoutHotelsResponse DTO for simplified city data transfer.

---
+++ TravelEase.Application/CityManagement/DTOs/Responses/CityWithoutHotelsResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelEase.Application.CityManagement.DTOs.Responses
+{
+    public class CityWithoutHotelsResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; } 
+        public string PostOffice { get; set; }
+    }
+}
