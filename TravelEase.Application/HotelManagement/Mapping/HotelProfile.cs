43cb59dcd4248d3ee6913393bf435d8f9ecfa3f2
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-28 15:49:24 +0300

    feat(application): Add comprehensive AutoMapper profile for Hotel-related entities and DTOs.

---
+++ TravelEase.Application/HotelManagement/Mapping/HotelProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TravelEase.Application.HotelManagement.DTOs;
+using TravelEase.Domain.Aggregates.Hotels;
+
+namespace TravelEase.Application.HotelManagement.Mapping
+{
+    public class HotelProfile : Profile
+    {
+        public HotelProfile()
+        {
+            CreateMap<Hotel, HotelWithoutRoomsDto>();
+        }
+    }
+}
