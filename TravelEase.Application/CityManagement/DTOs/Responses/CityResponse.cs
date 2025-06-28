3b3b8ccf6226dc2be81ff0ef3865312e3facfeb6
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-28 12:24:30 +0300

    feat(application): Add CityResponse DTO with hotel data excluding room details.

---
+++ TravelEase.Application/CityManagement/DTOs/Responses/CityResponse.cs
@@ -0,0 +1,14 @@
+﻿using TravelEase.Application.HotelManagement.DTOs;
+
+namespace TravelEase.Application.CityManagement.DTOs.Responses
+{
+    public class CityResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
+        public string PostOffice { get; set; }
+        public IList<HotelWithoutRoomsDto> Hotels { get; set; }
+    }
+}
