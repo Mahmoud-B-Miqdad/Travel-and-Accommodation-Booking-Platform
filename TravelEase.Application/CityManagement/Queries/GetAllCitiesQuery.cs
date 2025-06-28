9f2f0aa98f1966eb7d1a076213cf0a719fb205ef
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-28 12:34:57 +0300

    feat(application): Add GetCitiesQuery using MediatR with filtering and pagination parameters.

---
+++ TravelEase.Application/CityManagement/Queries/GetAllCitiesQuery.cs
@@ -0,0 +1,14 @@
+﻿using TravelEase.Application.CityManagement.DTOs.Responses;
+using TravelEase.Domain.Common.Models.PaginationModels;
+using MediatR;
+
+namespace TravelEase.Application.CityManagement.Queries
+{
+    public record GetCitiesQuery : IRequest<PaginatedList<CityResponse>>
+    {
+        public bool IncludeHotels { get; set; } = false;
+        public string? SearchQuery { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
