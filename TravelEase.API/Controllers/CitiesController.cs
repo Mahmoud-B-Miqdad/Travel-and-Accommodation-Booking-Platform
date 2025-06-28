6f59dd5e6efa5bd23d71e14d6d37ae8c6dcbcd66
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-28 16:27:15 +0300

    feat(api): Add CitiesController with endpoint to retrieve paginated list of cities using MediatR.

---
+++ TravelEase.API/Controllers/CitiesController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using TravelEase.API.Common.Responses;
+using TravelEase.Application.CityManagement.DTOs.Responses;
+using TravelEase.Application.CityManagement.Queries;
+
+namespace TravelEase.API.Controllers
+{
+    [Route("api/cities")]
+    [ApiController]
+    [ApiVersion("1.0")]
+
+    public class CitiesController : Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public CitiesController(IMediator mediator,IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAllCitiesAsync([FromQuery] GetAllCitiesQuery cityQuery)
+        {
+            var paginatedListOfCities = await _mediator.Send(cityQuery);
+            Response.Headers.Append("X-Pagination", 
+                JsonSerializer.Serialize(paginatedListOfCities.PageData));
+
+
+            if (!cityQuery.IncludeHotels)
+                return Ok(ApiResponse<object>.SuccessResponse(_mapper
+                 .Map<List<CityWithoutHotelsResponse>>
+                 (paginatedListOfCities.Items)));
+
+            return Ok(ApiResponse<object>.SuccessResponse(paginatedListOfCities.Items));
+        }
+    }
+}
