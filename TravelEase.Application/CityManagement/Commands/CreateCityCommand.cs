666946e07bbbb621ce51d7ca5013daad5d08d449
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-29 11:07:26 +0300

    feat(city): add CreateCityCommand for handling city creation requests.

---
+++ TravelEase.Application/CityManagement/Commands/CreateCityCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using TravelEase.Application.CityManagement.DTOs.Responses;
+
+namespace TravelEase.Application.CityManagement.Commands
+{
+    public record CreateCityCommand : IRequest<CityWithoutHotelsResponse?>
+    {
+        public string Name { get; set; }
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
+        public string PostOffice { get; set; }
+    }
+}
