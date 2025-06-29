6bfb40c263bcf54d02d28a973dbc44b9d3dba333
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-29 10:04:07 +0300

    feat(city): add GetCityByIdQuery for fetching city details by ID.

---
+++ TravelEase.Application/CityManagement/Queries/GetCityByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TravelEase.Application.CityManagement.DTOs.Responses;
+
+namespace TravelEase.Application.CityManagement.Queries
+{
+    public record GetCityByIdQuery : IRequest<CityResponse?>
+    {
+        public Guid Id { get; set; }
+    }
+}
