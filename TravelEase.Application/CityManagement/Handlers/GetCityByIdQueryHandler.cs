4433ad6dd48f0326b3f92beb5cacc557917edeea
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-29 10:14:44 +0300

    feat(application): add handler for GetCityById query with not-found exception handling.

---
+++ TravelEase.Application/CityManagement/Handlers/GetCityByIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using TravelEase.Application.CityManagement.DTOs.Responses;
+using TravelEase.Application.CityManagement.Queries;
+using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Domain.Exceptions;
+
+namespace TravelEase.Application.CityManagement.Handlers
+{
+    public class GetCityByIdQueryHandler : IRequestHandler<GetCityByIdQuery, CityResponse?>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetCityByIdQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<CityResponse?> Handle(GetCityByIdQuery request, CancellationToken cancellationToken)
+        {
+            var city = await _unitOfWork.Cities.GetByIdAsync(request.Id);
+            if (city == null)
+                throw new NotFoundException($"City with Id {request.Id} was not found.");
+
+            return _mapper.Map<CityResponse>(city);
+        }
+    }
+}
