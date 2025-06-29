a91000e6a5dc135c42e2b6477a6cd1e033566a0d
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-29 10:47:05 +0300

    feat(api): add GetCity endpoint to retrieve city details by ID.

--- TravelEase.API/Controllers/CitiesController.cs
+++ TravelEase.API/Controllers/CitiesController.cs
@@ -43,5 +43,18 @@ namespace TravelEase.API.Controllers
 
             return Ok(ApiResponse<object>.SuccessResponse(paginatedListOfCities.Items));
         }
+
+        [HttpGet("{cityId:guid}", Name = "GetCity")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetCityAsync(Guid cityId)
+        {
+            var request = new GetCityByIdQuery { Id = cityId};
+            var result = await _mediator.Send(request);
+            if (result is null) return NotFound();
+            return Ok(_mapper.Map<CityWithoutHotelsResponse>(result));
+        }
     }
 }
