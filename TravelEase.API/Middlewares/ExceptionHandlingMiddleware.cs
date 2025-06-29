cc0edc36a023bbf61e6c69d418c147af2be3eff1
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-29 10:29:10 +0300

    feat(api): add global ExceptionHandlingMiddleware for consistent error responses.

---
+++ TravelEase.API/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using TravelEase.API.Common.Responses;
+using TravelEase.Domain.Exceptions;
+using System.Text.Json;
+
+namespace TravelEase.API.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unhandled exception occurred.");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+
+            HttpStatusCode statusCode;
+            object apiResponse;
+
+            switch (exception)
+            {
+                case NotFoundException notFoundEx:
+                    statusCode = HttpStatusCode.NotFound;
+                    apiResponse = ApiResponse<string>.FailResponse(notFoundEx.Message);
+                    break;
+
+                default:
+                    statusCode = HttpStatusCode.InternalServerError;
+                    apiResponse = ApiResponse<string>.FailResponse("An unexpected error occurred.");
+                    break;
+            }
+
+            context.Response.StatusCode = (int)statusCode;
+            var json = JsonSerializer.Serialize(apiResponse);
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
--- TravelEase.API/Program.cs
+++ TravelEase.API/Program.cs
@@ -60,7 +60,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseMiddleware<ValidationExceptionHandlingMiddleware>();
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseAuthorization();
 
