a2ddcc3abf9a66a52540f7477dd8fa9af2caddcf
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-29 11:18:30 +0300

    feat(city): add CityForCreationRequest DTO to handle city creation input data.

---
+++ TravelEase.Application/CityManagement/DTOs/Requests/CityForCreationRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelEase.Application.CityManagement.DTOs.Requests
+{
+    public class CityForCreationRequest
+    {
+
+        public string Name { get; set; } = string.Empty;
+        public string CountryName { get; set; } = string.Empty;
+        public string CountryCode { get; set; } = string.Empty;
+        public string PostOffice { get; set; } = string.Empty;
+    }
+}
