27d7b20fd54363fa80be2964a011fe86f84c1818
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-29 14:53:38 +0300

    feat(Application): add DeleteCityHandler with exist check logic.

---
+++ TravelEase.Application/CityManagement/Handlers/DeleteCityHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using TravelEase.Application.CityManagement.Commands;
+using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Domain.Exceptions;
+
+namespace TravelEase.Application.CityManagement.Handlers
+{
+    public class DeleteCityHandler : IRequestHandler<DeleteCityCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public DeleteCityHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task Handle(DeleteCityCommand request, CancellationToken cancellationToken)
+        {
+            var existingCity = await _unitOfWork.Cities.GetByIdAsync(request.Id);
+            if (existingCity == null)
+            {
+                throw new NotFoundException("City Doesn't Exists To Delete");
+            }
+            _unitOfWork.Cities.Remove(existingCity);
+        }
+    }
+}
