d90ed8d4d57c233147ec50cb32b2f6ed075582f9
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-29 13:56:30 +0300

    feat(Application): add CityForUpdateRequest DTO to encapsulate city update data.

---
+++ TravelEase.Application/CityManagement/DTOs/Requests/CityForUpdateRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelEase.Application.CityManagement.DTOs.Requests
+{
+    public class CityForUpdateRequest
+    {
+        public string Name { get; set; }
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
+        public string PostOffice { get; set; }
+    }
+}
