515b72203bc54c59024b788547109ecb5bb71eee
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-30 14:21:27 +0300

    feat(api): add GetAllRoomAmenitiesQueryValidator for pagination validation.

---
+++ TravelEase.API/Validators/RoomAmenityValidators/GetAllRoomAmenitiesQueryValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using TravelEase.Application.RoomAmenity.Query;
+
+namespace TravelEase.API.Validators.RoomAmenityValidators
+{
+    public class GetAllRoomAmenitiesQueryValidator : AbstractValidator<GetAllRoomAmenitiesQuery>
+    {
+        public GetAllRoomAmenitiesQueryValidator()
+        {
+            RuleFor(roomAmenity => roomAmenity.PageNumber)
+                .GreaterThan(0)
+                .WithMessage("Page number must be greater than 0.");
+
+            RuleFor(roomAmenity => roomAmenity.PageSize)
+                .GreaterThan(0)
+                .WithMessage("Page size must be greater than 0.")
+                .LessThan(21)
+                .WithMessage("Page Size can't be greater than 20");
+        }
+    }
+}
