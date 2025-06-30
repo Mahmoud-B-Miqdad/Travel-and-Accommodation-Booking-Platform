5c980b0e39c974b7b7339a1efd79180dc73a21f5
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-30 10:23:08 +0300

    feat(Application): implement GetHotelByIdQueryHandler with not found exception handling.

---
+++ TravelEase.Application/HotelManagement/Handlers/GetHotelByIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using TravelEase.Application.HotelManagement.DTOs.Responses;
+using TravelEase.Application.HotelManagement.Queries;
+using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Domain.Exceptions;
+
+namespace TravelEase.Application.HotelManagement.Handlers
+{
+    public class GetHotelByIdQueryHandler : IRequestHandler<GetHotelByIdQuery, HotelWithoutRoomsResponse?>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetHotelByIdQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<HotelWithoutRoomsResponse?> Handle(GetHotelByIdQuery request, CancellationToken cancellationToken)
+        {
+            var hotel = await _unitOfWork.Hotels.GetByIdAsync(request.Id);
+            if (hotel == null)
+                throw new NotFoundException($"Hotel with Id {request.Id} was not found.");
+
+            return _mapper.Map<HotelWithoutRoomsResponse>(hotel);
+        }
+    }
+}
