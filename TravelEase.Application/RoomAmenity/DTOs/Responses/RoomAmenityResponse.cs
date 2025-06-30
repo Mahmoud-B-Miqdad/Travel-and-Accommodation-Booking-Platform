5a2f4e2daf99ace8919f8f29880982fadd1a15ad
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-30 14:13:45 +0300

    feat(Application): add RoomAmenityResponse DTO for room amenity data transfer.

---
+++ TravelEase.Application/RoomAmenity/DTOs/Responses/RoomAmenityResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelEase.Application.RoomAmenity.DTOs.Responses
+{
+    public record RoomAmenityResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
