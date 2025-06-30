bebd59dc0166d76dfbfa3861fe60980efecea02a
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-30 09:06:36 +0300

    feat(application): add GetAllHotelsQuery for paginated hotel retrieval with optional search.

---
+++ TravelEase.Application/HotelManagement/Queries/GetAllHotelsQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using TravelEase.Application.HotelManagement.DTOs;
+using TravelEase.Domain.Common.Models.PaginationModels;
+
+namespace TravelEase.Application.HotelManagement.Queries
+{
+    public class GetAllHotelsQuery : IRequest<PaginatedList<HotelWithoutRoomsDto>>
+    {
+        public string? SearchQuery { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
