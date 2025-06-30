4fe0ee3aca9126f9935f88bcb562f10e9f14c822
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-30 15:12:16 +0300

    feat(Application): add GetRoomAmenityByIdQuery for retrieving amenity details by ID.

---
+++ TravelEase.Application/RoomAmenityManagement/Query/GetRoomAmenityByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TravelEase.Application.RoomAmenityManagement.DTOs.Responses;
+
+namespace TravelEase.Application.RoomAmenityManagement.Query
+{
+    public record GetRoomAmenityByIdQuery : IRequest<RoomAmenityResponse?>
+    {
+        public Guid Id { get; set; }
+    }
+}
