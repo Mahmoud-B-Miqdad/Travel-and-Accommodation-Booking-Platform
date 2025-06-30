8d7458941a728a0655468c868e22d6e9a9d13a3f
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-30 10:38:18 +0300

    feat(hotel/application): add CreateHotelCommand to handle hotel creation request data.

---
+++ TravelEase.Application/HotelManagement/Commands/CreateHotelCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using TravelEase.Application.HotelManagement.DTOs.Responses;
+
+namespace TravelEase.Application.HotelManagement.Commands
+{
+    public class CreateHotelCommand : IRequest<HotelWithoutRoomsResponse?>
+    {
+        public Guid CityId { get; set; }
+        public Guid OwnerId { get; set; }
+        public string Name { get; set; }
+        public float Rating { get; set; }
+        public string StreetAddress { get; set; }
+        public string Description { get; set; }
+        public string PhoneNumber { get; set; }
+        public int FloorsNumber { get; set; }
+    }
+}
