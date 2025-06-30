4d91799b700a28672dd334207e97ed61b0e19de3
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-30 14:51:06 +0300

    refactor(Application): rename RoomAmenity folder to RoomAmenityManagement for clarity and consistency

---
+++ TravelEase.Application/RoomAmenityManagement/Mapping/RoomAmenityProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using TravelEase.Domain.Aggregates.RoomAmenities;
+
+namespace TravelEase.Application.RoomAmenityManagement.Mapping
+{
+    public class RoomAmenityProfile : Profile
+    {
+        public RoomAmenityProfile()
+        {
+            CreateMap<RoomAmenity, RoomAmenityProfile>();
+        }
+    }
+}
---
+++ TravelEase.Application/RoomAmenityManagement/Query/GetAllRoomAmenitiesQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using TravelEase.Application.RoomAmenityManagement.DTOs.Responses;
+using TravelEase.Domain.Common.Models.PaginationModels;
+
+namespace TravelEase.Application.RoomAmenityManagement.Query
+{
+    public record GetAllRoomAmenitiesQuery : IRequest<PaginatedList<RoomAmenityResponse>>
+    {
+        public string? SearchQuery { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
