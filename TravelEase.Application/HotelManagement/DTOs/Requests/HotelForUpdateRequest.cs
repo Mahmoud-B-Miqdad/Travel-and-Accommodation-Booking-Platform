1cc5312adb5354bfcb03ecac890bae8a7be8b12a
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-30 12:04:02 +0300

    feat(application): add HotelForUpdateRequest DTO for updating hotel details.

---
+++ TravelEase.Application/HotelManagement/DTOs/Requests/HotelForUpdateRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelEase.Application.HotelManagement.DTOs.Requests
+{
+    public record HotelForUpdateRequest
+    {
+        public Guid CityId { get; set; }
+        public string OwnerName { get; set; }
+        public string Name { get; set; }
+        public float Rating { get; set; }
+        public string StreetAddress { get; set; }
+        public string Description { get; set; }
+        public string PhoneNumber { get; set; }
+        public int FloorsNumber { get; set; }
+    }
+}
