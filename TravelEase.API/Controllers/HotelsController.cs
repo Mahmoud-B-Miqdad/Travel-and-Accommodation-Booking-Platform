670b21799d06b7ef5d1416df25de8f7e107eba1b
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-30 10:33:18 +0300

    feat(api): add GetHotelAsync endpoint to retrieve hotel by ID with proper responses.

--- TravelEase.API/Controllers/HotelsController.cs
+++ TravelEase.API/Controllers/HotelsController.cs
@@ -6,6 +6,7 @@ using TravelEase.Application.HotelManagement.Queries;
 using TravelEase.API.Common.Responses;
 using TravelEase.Application.CityManagement.DTOs.Responses;
 using TravelEase.Application.HotelManagement.DTOs.Responses;
+using Microsoft.AspNetCore.Authorization;
 
 namespace TravelEase.API.Controllers
 {
@@ -45,6 +46,28 @@ namespace TravelEase.API.Controllers
                 JsonSerializer.Serialize(paginatedListOfHotels.PageData));
 
             return Ok(ApiResponse<List<HotelWithoutRoomsResponse>>.SuccessResponse(paginatedListOfHotels.Items));
+        }
+
+        /// <summary>
+        /// Retrieves information about a hotel based on its unique identifier (GUID).
+        /// </summary>
+        /// <param name="hotelId">The unique identifier of the hotel.</param>
+        /// <returns>
+        /// - 200 OK: If the hotel information is successfully retrieved.
+        /// - 404 Not Found: If the hotel with the given ID does not exist.
+        /// - 500 Internal Server Error: If an unexpected error occurs.
+        /// </returns>
+        [HttpGet("{hotelId:guid}", Name = "GetHotel")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetHotelAsync(Guid hotelId)
+        {
+            var request = new GetHotelByIdQuery { Id = hotelId };
+            var hotel = await _mediator.Send(request);
+
+            return Ok(ApiResponse<HotelWithoutRoomsResponse>.SuccessResponse(hotel!));
         }
     }
 }
