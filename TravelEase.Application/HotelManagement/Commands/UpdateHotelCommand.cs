775007c4ce6dd87dc2d64623677bdfd74bb1c0b6
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-30 12:02:52 +0300

    feat(application): add UpdateHotelCommand for hotel update requests via MediatR.

---
+++ TravelEase.Application/HotelManagement/Commands/UpdateHotelCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace TravelEase.Application.HotelManagement.Commands
+{
+    public class UpdateHotelCommand : IRequest
+    {
+        public Guid Id { get; set; }
+        public Guid CityId { get; set; }
+        public string OwnerName { get; set; }
+        public string Name { get; set; }
+        public float Rating { get; set; }
+        public string StreetAddress { get; set; }
+        public string Description { get; set; }
+        public string PhoneNumber { get; set; }
+        public int FloorsNumber { get; set; }
+    }
+}
