24811369326b56fbd98fa55b7f4b5298825aaad2
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-30 16:16:11 +0300

    feat(Application): add CreateRoomAmenityCommand for room amenity creation.

---
+++ TravelEase.Application/RoomAmenityManagement/Commands/CreateRoomAmenityCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using TravelEase.Application.RoomAmenityManagement.DTOs.Responses;
+
+namespace TravelEase.Application.RoomAmenityManagement.Commands
+{
+    public record CreateRoomAmenityCommand : IRequest<RoomAmenityResponse?>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
