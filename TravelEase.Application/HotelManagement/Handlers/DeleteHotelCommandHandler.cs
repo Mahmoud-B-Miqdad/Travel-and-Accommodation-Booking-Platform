06479f61efe23eada755a7474c798b6b33c81604
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-30 13:28:33 +0300

    feat(Application): implement DeleteHotelCommandHandler with existence check and delete operation.

---
+++ TravelEase.Application/HotelManagement/Handlers/DeleteHotelCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using TravelEase.Application.HotelManagement.Commands;
+using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Domain.Exceptions;
+
+namespace TravelEase.Application.HotelManagement.Handlers
+{
+    public class DeleteHotelCommandHandler : IRequestHandler<DeleteHotelCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public DeleteHotelCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task Handle(DeleteHotelCommand request, CancellationToken cancellationToken)
+        {
+
+            var existingHotel = await _unitOfWork.Hotels.GetByIdAsync(request.Id);
+            if (existingHotel == null)
+                throw new NotFoundException("Hotel Doesn't Exists To Delete");
+
+            _unitOfWork.Hotels.Remove(existingHotel);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
