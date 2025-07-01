d19b70cb5e747d2263d9224e40638b4474f646b7
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-01 14:06:20 +0300

    feat(infrastructure/domain): implement JWT token generator with credential validation and claims setup.

---
+++ TravelEase.Domain/Common/Interfaces/ITokenGenerator.cs
@@ -0,0 +1,14 @@
+﻿using TravelEase.Domain.Aggregates.Users;
+
+namespace TravelEase.Domain.Common.Interfaces
+{
+    public interface ITokenGenerator
+    {
+        public Task<string> GenerateToken(
+            string email,
+            string password,
+            string secretKey,
+            string issuer,
+            string audience);
+    }
+}
