87251f321780008c7701d77a6d451ebaf95ecf2c
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-01 08:26:48 +0300

    feat(application): add UpdateRoomAmenityCommand for updating room amenity information.

---
+++ TravelEase.Application/RoomAmenityManagement/Commands/UpdateRoomAmenityCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace TravelEase.Application.RoomAmenityManagement.Commands
+{
+    public record UpdateRoomAmenityCommand : IRequest
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
