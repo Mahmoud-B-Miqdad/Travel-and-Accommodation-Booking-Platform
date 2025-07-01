a62075013e51cc2980e47ff92907a8da33b85db1
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-01 10:14:27 +0300

    feat(application): add GetAllReviewsByHotelIdQuery with pagination and search support.

---
+++ TravelEase.Application/ReviewsManagement/Queries/GetAllReviewsByHotelIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using TravelEase.Application.ReviewsManagement.DTOs.Responses;
+using TravelEase.Domain.Common.Models.PaginationModels;
+
+namespace TravelEase.Application.ReviewsManagement.Queries
+{
+    public class GetAllReviewsByHotelIdQuery : IRequest<PaginatedList<ReviewResponse>>
+    {
+        public Guid HotelId { get; set; }
+        public string? SearchQuery { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
