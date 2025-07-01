fb46ea0f38f7aa8eed4962a1c9e85f16c6c48c53
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-01 10:13:04 +0300

    feat(application): add ReviewResponse DTO for exposing review data.

---
+++ TravelEase.Application/ReviewsManagement/DTOs/Responses/ReviewResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelEase.Application.ReviewsManagement.DTOs.Responses
+{
+    public record ReviewResponse
+    {
+        public Guid Id { get; set; }
+        public string Comment { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public float Rating { get; set; }
+    }
+}
