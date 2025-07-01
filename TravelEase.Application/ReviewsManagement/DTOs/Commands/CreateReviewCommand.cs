b1f2d8573e31af2c51864d906a330db8669a4d3a
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-01 10:55:44 +0300

    feat(application): add CreateReviewCommand for creating new review via MediatR.

---
+++ TravelEase.Application/ReviewsManagement/DTOs/Commands/CreateReviewCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using TravelEase.Application.ReviewsManagement.DTOs.Responses;
+
+namespace TravelEase.Application.ReviewsManagement.DTOs.Commands
+{
+    public class CreateReviewCommand : IRequest<ReviewResponse?>
+    {
+        public Guid BookingId { get; set; }
+        public string Comment { get; set; }
+        public DateTime ReviewDate { get; set; } = DateTime.Today;
+        public float Rating { get; set; }
+    }
+}
