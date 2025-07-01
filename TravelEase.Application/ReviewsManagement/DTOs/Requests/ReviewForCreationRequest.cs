e0e29ccd24ec3af295c4b02cd86b6602805ba6ef
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-01 10:50:57 +0300

    feat(application): add ReviewForCreationRequest DTO for creating new reviews.

---
+++ TravelEase.Application/ReviewsManagement/DTOs/Requests/ReviewForCreationRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelEase.Application.ReviewsManagement.DTOs.Requests
+{
+    public record ReviewForCreationRequest
+    {
+        public Guid BookingId { get; set; }
+        public string Comment { get; set; }
+        public float Rating { get; set; }
+    }
+}
