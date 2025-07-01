aa45e578376e2f2b5f7524b26ac5571e4073aff3
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-01 14:17:47 +0300

    feat(application): add SignInRequest DTO for user login input.

---
+++ TravelEase.Application/UserManagement/DTOs/Requests/SignInRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace TravelEase.Application.UserManagement.DTOs.Requests
+{
+    public class SignInRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
