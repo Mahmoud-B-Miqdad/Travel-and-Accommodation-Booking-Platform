81d9cf106c91957a4e4133ea58af9928ad8fdf8c
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-01 14:19:51 +0300

    feat(api): add FluentValidation for SignInRequest with password complexity rules.

---
+++ TravelEase.API/Validators/AuthValidators/SignInRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using TravelEase.Application.UserManagement.DTOs.Requests;
+
+namespace TravelEase.API.Validators.AuthValidators
+{
+    public class SignInRequestValidator : AbstractValidator<SignInRequest>
+    {
+        public SignInRequestValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(8).WithMessage("Password must be at least 8 characters.")
+                .Matches("[A-Z]").WithMessage("Password must contain an uppercase letter.")
+                .Matches("[a-z]").WithMessage("Password must contain a lowercase letter.")
+                .Matches("[0-9]").WithMessage("Password must contain a digit.")
+                .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain a special character.");
+        }
+    }
+}
