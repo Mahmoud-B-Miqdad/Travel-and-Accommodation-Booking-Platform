8bf84bef239212149e3ec8609de30a09d17a7ebf
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-01 10:39:55 +0300

    feat(api): add endpoint to retrieve paginated reviews by hotel ID with filtering support.

---
+++ TravelEase.API/Controllers/ReviewsController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using TravelEase.API.Common.Responses;
+using TravelEase.Application.ReviewsManagement.DTOs.Requests;
+using TravelEase.Application.ReviewsManagement.DTOs.Responses;
+using TravelEase.Application.ReviewsManagement.Queries;
+
+namespace TravelEase.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public ReviewsController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Retrieves a paginated list of reviews for a specific hotel.
+        /// </summary>
+        /// <param name="hotelId">The ID of the hotel for which reviews are requested.</param>
+        /// <param name="reviewQueryDto">DTO containing parameters for pagination and filtering.</param>
+        /// <returns>
+        /// Returns a paginated list of reviews for the specified hotel.
+        /// </returns>
+        /// <response code="200">Returns a paginated list of reviews.</response>
+        [HttpGet("hotels/{hotelId:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllReviewsByHotelIdAsync(Guid hotelId,
+            [FromQuery] ReviewQueryRequest reviewQueryDto)
+        {
+            var reviewQuery = _mapper.Map<GetAllReviewsByHotelIdQuery>(reviewQueryDto);
+            reviewQuery.HotelId = hotelId;
+
+            var paginatedListOfReviews = await _mediator.Send(reviewQuery);
+            Response.Headers.Append("X-Pagination",
+                JsonSerializer.Serialize(paginatedListOfReviews.PageData));
+
+            return Ok(ApiResponse<List<ReviewResponse>>.SuccessResponse(paginatedListOfReviews.Items));
+        }
+    }
+}
--- TravelEase.Application/ReviewsManagement/Mapping/ReviewsProfile.cs
+++ TravelEase.Application/ReviewsManagement/Mapping/ReviewsProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using TravelEase.Application.ReviewsManagement.DTOs.Requests;
 using TravelEase.Application.ReviewsManagement.DTOs.Responses;
+using TravelEase.Application.ReviewsManagement.Queries;
 using TravelEase.Domain.Aggregates.Reviews;
 
 namespace TravelEase.Application.ReviewsManagement.Mapping
@@ -9,6 +11,7 @@ namespace TravelEase.Application.ReviewsManagement.Mapping
         public ReviewsProfile()
         {
             CreateMap<Review, ReviewResponse>();
+            CreateMap<ReviewQueryRequest, GetAllReviewsByHotelIdQuery>();
         }
     }
 }
