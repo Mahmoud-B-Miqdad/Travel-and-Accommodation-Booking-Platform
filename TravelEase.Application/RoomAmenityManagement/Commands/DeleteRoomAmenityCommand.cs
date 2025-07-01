1abe1889ab7e20e64311a63dec0fe0cc98d2166b
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-01 08:55:45 +0300

    feat(room-amenity): add DeleteRoomAmenityCommand for deleting room amenities by Id.

---
+++ TravelEase.Application/RoomAmenityManagement/Commands/DeleteRoomAmenityCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace TravelEase.Application.RoomAmenityManagement.Commands
+{
+    public record DeleteRoomAmenityCommand : IRequest
+    {
+        public Guid Id { get; set; }
+    }
+}
