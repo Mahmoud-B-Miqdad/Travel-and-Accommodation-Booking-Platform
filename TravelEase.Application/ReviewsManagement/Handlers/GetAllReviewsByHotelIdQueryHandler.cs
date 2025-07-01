7fc9bae0f47dcd0c23f4c0f87a685ccb20fb493a
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-01 10:26:19 +0300

    feat(application): implement GetAllReviewsByHotelIdQueryHandler with mapping and pagination support.

---
+++ TravelEase.Application/ReviewsManagement/Handlers/GetAllReviewsByHotelIdQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using MediatR;
+using TravelEase.Application.ReviewsManagement.DTOs.Responses;
+using TravelEase.Application.ReviewsManagement.Queries;
+using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Domain.Common.Models.PaginationModels;
+
+namespace TravelEase.Application.ReviewsManagement.Handlers
+{
+    internal class GetAllReviewsByHotelIdQueryHandler : 
+        IRequestHandler<GetAllReviewsByHotelIdQuery, PaginatedList<ReviewResponse>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetAllReviewsByHotelIdQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<PaginatedList<ReviewResponse>> Handle
+            (GetAllReviewsByHotelIdQuery request,
+            CancellationToken cancellationToken)
+        {
+            var paginatedList = await
+                _unitOfWork.Reviews
+                    .GetAllByHotelIdAsync(
+                        request.HotelId,
+                        request.SearchQuery,
+                        request.PageNumber,
+                        request.PageSize);
+
+            return new PaginatedList<ReviewResponse>(
+                _mapper.Map<List<ReviewResponse>>(paginatedList.Items),
+                paginatedList.PageData);
+        }
+    }
+}
---
+++ TravelEase.Application/ReviewsManagement/Mapping/ReviewsProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TravelEase.Application.ReviewsManagement.DTOs.Responses;
+using TravelEase.Domain.Aggregates.Reviews;
+
+namespace TravelEase.Application.ReviewsManagement.Mapping
+{
+    public class ReviewsProfile : Profile
+    {
+        public ReviewsProfile()
+        {
+            CreateMap<Review, ReviewResponse>();
+        }
+    }
+}
