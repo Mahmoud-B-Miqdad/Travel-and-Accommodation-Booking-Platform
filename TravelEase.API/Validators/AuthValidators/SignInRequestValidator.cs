4cc429aa58d1f4e39d381276e5b43d7de1515e85
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-01 15:16:52 +0300

    refactor(api): simplify SignInRequest validator by removing password complexity rules.

--- TravelEase.API/Validators/AuthValidators/SignInRequestValidator.cs
+++ TravelEase.API/Validators/AuthValidators/SignInRequestValidator.cs
@@ -12,12 +12,7 @@ namespace TravelEase.API.Validators.AuthValidators
                 .EmailAddress().WithMessage("Invalid email format.");
 
             RuleFor(x => x.Password)
-                .NotEmpty().WithMessage("Password is required.")
-                .MinimumLength(8).WithMessage("Password must be at least 8 characters.")
-                .Matches("[A-Z]").WithMessage("Password must contain an uppercase letter.")
-                .Matches("[a-z]").WithMessage("Password must contain a lowercase letter.")
-                .Matches("[0-9]").WithMessage("Password must contain a digit.")
-                .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain a special character.");
+                .NotEmpty().WithMessage("Password is required.");
         }
     }
 }
