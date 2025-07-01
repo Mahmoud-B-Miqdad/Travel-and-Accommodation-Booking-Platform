dc7b00248ac831464caca782e254b92fecdae43c
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-01 12:37:08 +0300

    feat(application): add CreateUserCommand for user registration via MediatR.

---
+++ TravelEase.Application/UserManagement/Commands/CreateUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace TravelEase.Application.UserManagement.Commands
+{
+    public record CreateUserCommand : IRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
