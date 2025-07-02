23cac9f1f0d21a012aa6f0705461b93a472030da
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-02 09:34:17 +0300

    feat(application): add ReserveRoomRequest DTO for booking room reservations.

---
+++ TravelEase.Application/BookingManagement/DTOs/Requests/ReserveRoomRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelEase.Application.BookingManagement.DTOs.Requests
+{
+    public record ReserveRoomRequest
+    {
+        public Guid RoomId { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+    }
+}
