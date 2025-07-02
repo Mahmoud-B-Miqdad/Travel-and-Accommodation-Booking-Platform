540284872d4686a65647193b6a3bf02130b61109
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-02 09:41:31 +0300

    feat(application): add BookingResponse DTO for returning booking details.

---
+++ TravelEase.Application/BookingManagement/DTOs/Responses/BookingResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelEase.Application.BookingManagement.DTOs.Responses
+{
+    public record BookingResponse
+    {
+        public Guid Id { get; set; }
+        public Guid RoomId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public DateTime BookingDate { get; set; }
+    }
+}
