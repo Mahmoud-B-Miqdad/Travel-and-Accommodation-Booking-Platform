d0c28b57d13d4a2e1362f7fa44f6bbd8ec069020
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-02 09:37:20 +0300

    feat(api): add FluentValidation for ReserveRoomRequest with date validation rules.

---
+++ TravelEase.API/Validators/BookingValidators/ReserveRoomRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using TravelEase.Application.BookingManagement.DTOs.Requests;
+
+namespace TravelEase.API.Validators.BookingValidators
+{
+    public class ReserveRoomRequestValidator : AbstractValidator<ReserveRoomRequest>
+    {
+        public ReserveRoomRequestValidator()
+        {
+            RuleFor(booking => booking.CheckInDate)
+                .GreaterThan(DateTime.Today)
+                .WithMessage("Can't book a room in the same day or in the past for check-in");
+
+            RuleFor(booking => booking.CheckOutDate)
+                .GreaterThanOrEqualTo(booking => booking.CheckInDate.AddDays(1))
+                .WithMessage("Check-out date must be at least one day after the check-in date");
+        }
+    }
+}
