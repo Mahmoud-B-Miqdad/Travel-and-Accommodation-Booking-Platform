8f819e0b1504208d354d837efe05fbd11eef1d81
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-02 14:09:26 +0300

    feat(application): enhance DeleteBookingCommandHandler with guest access and check-in date validation.

--- TravelEase.Application/BookingManagement/Handlers/DeleteBookingCommandHandler.cs
+++ TravelEase.Application/BookingManagement/Handlers/DeleteBookingCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using TravelEase.Application.BookingManagement.Commands;
+using TravelEase.Domain.Aggregates.Bookings;
 using TravelEase.Domain.Common.Interfaces;
 using TravelEase.Domain.Exceptions;
 
@@ -18,6 +19,15 @@ namespace TravelEase.Application.BookingManagement.Handlers
             var existingBooking = await _unitOfWork.Bookings.GetByIdAsync(request.Id);
             if (existingBooking == null)
                 throw new NotFoundException("Booking Doesn't Exists To Delete");
+
+            var isAccessible = await _unitOfWork.Bookings.IsBookingAccessibleToUserAsync(
+            request.Id, request.GuestEmail);
+
+            if (!isAccessible)
+                throw new UnauthorizedAccessException("You are not authorized to cancel this booking");
+
+            if (existingBooking.CheckInDate <= DateTime.UtcNow.Date)
+                throw new BookingCheckInDatePassedException("Cannot cancel booking after check-in date");
 
             _unitOfWork.Bookings.Remove(existingBooking);
             await _unitOfWork.SaveChangesAsync(cancellationToken);
