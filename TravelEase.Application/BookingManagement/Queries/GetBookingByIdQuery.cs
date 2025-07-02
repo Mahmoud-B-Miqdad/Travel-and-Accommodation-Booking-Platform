b507765d09e8242c028861196048e44251ad03ad
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-02 11:13:04 +0300

    feat(application): add GetBookingByIdQuery for retrieving booking details by ID.

---
+++ TravelEase.Application/BookingManagement/Queries/GetBookingByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TravelEase.Application.BookingManagement.DTOs.Responses;
+
+namespace TravelEase.Application.BookingManagement.Queries
+{
+    public record GetBookingByIdQuery : IRequest<BookingResponse?>
+    {
+        public Guid Id { get; set; }
+    }
+}
