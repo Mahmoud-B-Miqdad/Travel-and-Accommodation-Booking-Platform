25b16ed2e3f6fc3109b1bb6a9136a78cd4a0509a
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-02 13:32:05 +0300

    feat(application): implement DeleteBookingCommandHandler to handle booking deletion.

---
+++ TravelEase.Application/BookingManagement/Handlers/DeleteBookingCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using TravelEase.Application.BookingManagement.Commands;
+using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Domain.Exceptions;
+
+namespace TravelEase.Application.BookingManagement.Handlers
+{
+    public class DeleteBookingCommandHandler : IRequestHandler<DeleteBookingCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public DeleteBookingCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(DeleteBookingCommand request, CancellationToken cancellationToken)
+        {
+            var existingBooking = await _unitOfWork.Bookings.GetByIdAsync(request.Id);
+            if (existingBooking == null)
+                throw new NotFoundException("Booking Doesn't Exists To Delete");
+
+            _unitOfWork.Bookings.Remove(existingBooking);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
