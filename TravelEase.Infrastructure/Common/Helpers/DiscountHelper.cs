969ede65ca1d7ce950e83864bfea1d6af8df201a
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-02 10:01:07 +0300

    feat(infrastructure): add DiscountHelper to calculate active room type discounts.

---
+++ TravelEase.Infrastructure/Common/Helpers/DiscountHelper.cs
@@ -0,0 +1,16 @@
+﻿using TravelEase.Domain.Aggregates.Discounts;
+
+namespace TravelEase.Infrastructure.Common.Helpers
+{
+    public static class DiscountHelper
+    {
+        public static float GetActiveDiscount(List<Discount> roomTypeDiscount)
+        {
+            return roomTypeDiscount
+                .FirstOrDefault(discount =>
+                    discount.FromDate.Date <= DateTime.Today.Date &&
+                    discount.ToDate.Date >= DateTime.Today.Date)
+                ?.DiscountPercentage ?? 0.0f;
+        }
+    }
+}
