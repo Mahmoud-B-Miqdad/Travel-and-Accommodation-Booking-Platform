9db033a0a48aa87dc5acd908625149d971e60279
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-02 11:20:17 +0300

    feat(application): implement GetBookingByIdQueryHandler to retrieve and map booking by ID.

---
+++ TravelEase.Application/BookingManagement/Handlers/GetBookingByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using TravelEase.Application.BookingManagement.DTOs.Responses;
+using TravelEase.Application.BookingManagement.Queries;
+using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Domain.Exceptions;
+
+namespace TravelEase.Application.BookingManagement.Handlers
+{
+    public class GetBookingByIdQueryHandler : IRequestHandler<GetBookingByIdQuery, BookingResponse?>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public GetBookingByIdQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<BookingResponse?> Handle(GetBookingByIdQuery request, CancellationToken cancellationToken)
+        {
+            var booking = await _unitOfWork.Bookings.GetByIdAsync(request.Id);
+            if (booking == null)
+                throw new NotFoundException($"Booking with Id {request.Id} was not found.");
+
+            return _mapper.Map<BookingResponse>(booking);
+        }
+    }
+}
