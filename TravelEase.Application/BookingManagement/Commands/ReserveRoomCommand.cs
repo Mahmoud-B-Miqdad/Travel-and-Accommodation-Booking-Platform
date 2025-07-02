1fe6c6ccb616bdb71be6e3d1985a7f582535adc2
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-02 09:44:52 +0300

    feat(application): add ReserveRoomCommand for creating new room booking via MediatR.

---
+++ TravelEase.Application/BookingManagement/Commands/ReserveRoomCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using TravelEase.Application.BookingManagement.DTOs.Responses;
+
+namespace TravelEase.Application.BookingManagement.Commands
+{
+    public record ReserveRoomCommand : IRequest<BookingResponse?>
+    {
+        public Guid RoomId { get; set; }
+        public string GuestEmail { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public DateTime BookingDate { get; set; } = DateTime.Now;
+    }
+}
