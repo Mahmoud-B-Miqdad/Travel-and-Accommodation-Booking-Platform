9114d5e85b29c2602323adb104cba25312697600
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-02 09:04:30 +0300

    feat(application): enhance CreateReviewCommandHandler with booking validation and duplicate review check.

--- TravelEase.Application/ReviewsManagement/Handlers/CreateReviewCommandHandler.cs
+++ TravelEase.Application/ReviewsManagement/Handlers/CreateReviewCommandHandler.cs
@@ -4,6 +4,7 @@ using TravelEase.Application.ReviewsManagement.DTOs.Commands;
 using TravelEase.Application.ReviewsManagement.DTOs.Responses;
 using TravelEase.Domain.Aggregates.Reviews;
 using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Domain.Exceptions;
 
 namespace TravelEase.Application.ReviewsManagement.Handlers
 {
@@ -19,6 +20,19 @@ namespace TravelEase.Application.ReviewsManagement.Handlers
 
         public async Task<ReviewResponse?> Handle(CreateReviewCommand request, CancellationToken cancellationToken)
         {
+            var bookingExists = await _unitOfWork.Bookings.IsExistsAsync(request.BookingId);
+            if (!bookingExists)
+                throw new NotFoundException($"Booking with ID {request.BookingId} does not exist.");
+
+            var isAuthorized = await _unitOfWork.Bookings.IsBookingAccessibleToUserAsync
+                (request.BookingId, request.GuestEmail!);
+            if (!isAuthorized)
+                throw new UnauthorizedAccessException("The authenticated user is not the one who booked this room.");
+
+            var reviewExists = await _unitOfWork.Reviews.IsExistsForBookingAsync(request.BookingId);
+            if (reviewExists)
+                throw new ConflictException("You already submitted a review for this booking.");
+
             var reviewToAdd = _mapper.Map<Review>(request);
             var addedReview = await _unitOfWork.Reviews.AddAsync(reviewToAdd);
 
