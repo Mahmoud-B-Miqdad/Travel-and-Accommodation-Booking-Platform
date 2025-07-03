4db7bebac2a06c57ac63b15802e62f3f43c2daf7
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-03 11:21:50 +0300

    feat(application): add GetReviewByIdQuery for fetching review details by ID.

---
+++ TravelEase.Application/ReviewsManagement/Queries/GetReviewByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TravelEase.Application.ReviewsManagement.DTOs.Responses;
+
+namespace TravelEase.Application.ReviewsManagement.Queries
+{
+    public class GetReviewByIdQuery : IRequest<ReviewResponse>
+    {
+        public Guid Id { get; set; }
+    }
+}
