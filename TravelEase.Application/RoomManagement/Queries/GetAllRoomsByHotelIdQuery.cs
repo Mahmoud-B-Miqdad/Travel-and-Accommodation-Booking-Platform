4797a6f2c5a4d28ac7b5181dbfa4b96e22ebc195
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-03 12:11:32 +0300

    feat(application): add GetAllRoomsByHotelIdQuery with pagination and search support.

---
+++ TravelEase.Application/RoomManagement/Queries/GetAllRoomsByHotelIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using TravelEase.Application.RoomManagement.DTOs.Responses;
+using TravelEase.Domain.Common.Models.PaginationModels;
+
+namespace TravelEase.Application.RoomManagement.Queries
+{
+    internal class GetAllRoomsByHotelIdQuery : IRequest<PaginatedList<RoomResponse>>
+    {
+        public Guid HotelId { get; set; }
+        public string? SearchQuery { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
