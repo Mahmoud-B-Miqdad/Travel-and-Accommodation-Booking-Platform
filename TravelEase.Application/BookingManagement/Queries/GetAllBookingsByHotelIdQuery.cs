a81753a41bb3b1250a404e636ef353f126ad367b
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-03 09:34:39 +0300

    feat(application): add GetAllBookingsByHotelIdQuery with pagination support.

---
+++ TravelEase.Application/BookingManagement/Queries/GetAllBookingsByHotelIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using TravelEase.Application.BookingManagement.DTOs.Responses;
+using TravelEase.Domain.Common.Models.PaginationModels;
+
+namespace TravelEase.Application.BookingManagement.Queries
+{
+    public class GetAllBookingsByHotelIdQuery : IRequest<PaginatedList<BookingResponse>>
+    {
+        public Guid HotelId { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
