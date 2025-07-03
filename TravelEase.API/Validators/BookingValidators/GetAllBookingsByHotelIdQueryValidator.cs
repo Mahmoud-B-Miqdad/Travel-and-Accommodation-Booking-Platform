b0815962dd5f4323295d6c16ce06b7be3dd6e305
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-03 09:38:15 +0300

    feat(api): add validation for GetAllBookingsByHotelIdQuery pagination parameters.

---
+++ TravelEase.API/Validators/BookingValidators/GetAllBookingsByHotelIdQueryValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using TravelEase.Application.BookingManagement.Queries;
+
+namespace TravelEase.API.Validators.BookingValidators
+{
+    public class GetAllBookingsByHotelIdQueryValidator : AbstractValidator<GetAllBookingsByHotelIdQuery>
+    {
+        public GetAllBookingsByHotelIdQueryValidator()
+        {
+            RuleFor(booking => booking.PageNumber)
+                .GreaterThan(0)
+                .WithMessage("Page number must be greater than 0.");
+
+            RuleFor(booking => booking.PageSize)
+                .GreaterThan(0)
+                .WithMessage("Page size must be greater than 0.")
+                .LessThan(21)
+                .WithMessage("Page Size can't be greater than 20");
+        }
+    }
+}
