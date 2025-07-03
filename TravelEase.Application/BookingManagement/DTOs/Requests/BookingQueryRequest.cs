af25cd732dd4cef4841c233071a43eb16dd79edf
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-03 09:51:10 +0300

    feat(application): add BookingQueryRequest DTO with default pagination properties.

---
+++ TravelEase.Application/BookingManagement/DTOs/Requests/BookingQueryRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace TravelEase.Application.BookingManagement.DTOs.Requests
+{
+    public class BookingQueryRequest
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
