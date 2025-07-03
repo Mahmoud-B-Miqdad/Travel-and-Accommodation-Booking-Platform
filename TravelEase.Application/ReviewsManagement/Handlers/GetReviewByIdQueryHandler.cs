bc960fbf196fe9665940ffdf60149a9f9078d916
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-03 11:25:27 +0300

    feat(application): add handler for GetReviewByIdQuery with not found check.

---
+++ TravelEase.Application/ReviewsManagement/Handlers/GetReviewByIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using TravelEase.Application.ReviewsManagement.DTOs.Responses;
+using TravelEase.Application.ReviewsManagement.Queries;
+using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Domain.Exceptions;
+
+namespace TravelEase.Application.ReviewsManagement.Handlers
+{
+    public class GetReviewByIdQueryHandler : IRequestHandler<GetReviewByIdQuery, ReviewResponse?>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetReviewByIdQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<ReviewResponse?> Handle(GetReviewByIdQuery request, CancellationToken cancellationToken)
+        {
+            var review = await _unitOfWork.Reviews.GetByIdAsync(request.Id);
+            if (review == null)
+                throw new NotFoundException($"Review with Id {request.Id} was not found.");
+
+            return _mapper.Map<ReviewResponse>(review);
+        }
+    }
+}
