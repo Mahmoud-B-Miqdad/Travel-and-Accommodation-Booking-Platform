0e5108bb5ca66deef0f670819f63087249ceb93a
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-03 12:07:50 +0300

    feat(application): add RoomResponse DTO for room details in responses.

---
+++ TravelEase.Application/RoomManagement/DTOs/Responses/RoomResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelEase.Application.RoomManagement.DTOs.Responses
+{
+    public record RoomResponse
+    {
+        public Guid Id { get; set; }
+        public Guid RoomTypeId { get; set; }
+        public int AdultsCapacity { get; set; }
+        public int ChildrenCapacity { get; set; }
+        public string View { get; set; }
+        public float Rating { get; set; }
+    }
+}
