b546e1567f01e28383417506076ae1b8266ed657
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-03 14:13:49 +0300

    feat(application): implement GetRoomByIdQueryHandler to fetch room by ID with exception handling.

--- TravelEase.Application/RoomManagement/Queries/GetRoomByIdQuery.cs
+++ TravelEase.Application/RoomManagement/Queries/GetRoomByIdQuery.cs
@@ -3,8 +3,8 @@ using TravelEase.Application.RoomManagement.DTOs.Responses;
 
 namespace TravelEase.Application.RoomManagement.Queries
 {
-    internal class GetRoomByIdQuery : IRequest<RoomResponse?>
+    public class GetRoomByIdQuery : IRequest<RoomResponse?>
     {
-        public Guid RoomId { get; set; }
+        public Guid Id { get; set; }
     }
 }
