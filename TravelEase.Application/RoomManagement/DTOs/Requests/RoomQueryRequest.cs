bc6d4c5279c6f79551eed814f265811b1b83f4e7
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-03 13:08:57 +0300

    feat(application): add RoomQueryRequest DTO to support pagination and search filters.

---
+++ TravelEase.Application/RoomManagement/DTOs/Requests/RoomQueryRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelEase.Application.RoomManagement.DTOs.Requests
+{
+    public class RoomQueryRequest
+    {
+        public string? SearchQuery { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
