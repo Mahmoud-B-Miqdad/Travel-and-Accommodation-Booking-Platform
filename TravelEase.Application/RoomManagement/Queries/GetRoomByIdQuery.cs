e1aa53c512d2768af9fab2bc220ab8d151e43c2a
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-03 13:39:26 +0300

    feat(application): add GetRoomByIdQuery to retrieve room details by ID.

---
+++ TravelEase.Application/RoomManagement/Queries/GetRoomByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TravelEase.Application.RoomManagement.DTOs.Responses;
+
+namespace TravelEase.Application.RoomManagement.Queries
+{
+    internal class GetRoomByIdQuery : IRequest<RoomResponse?>
+    {
+        public Guid RoomId { get; set; }
+    }
+}
