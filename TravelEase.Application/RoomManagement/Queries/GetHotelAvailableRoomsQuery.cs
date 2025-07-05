42651ad9c0d7cc1865593ca2f3e71bf88f04c260
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-05 13:48:56 +0300

    feat(application): add GetHotelAvailableRoomsQuery for fetching available rooms by date range and hotel ID.

---
+++ TravelEase.Application/RoomManagement/Queries/GetHotelAvailableRoomsQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using TravelEase.Application.RoomManagement.DTOs.Responses;
+
+namespace TravelEase.Application.RoomManagement.Queries
+{
+    public class GetHotelAvailableRoomsQuery : IRequest<List<RoomResponse>>
+    {
+        public Guid HotelId { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+    }
+}
