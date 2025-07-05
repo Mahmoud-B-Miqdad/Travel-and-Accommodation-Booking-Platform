587b1225839361eaa5cd292d1e0748a966552a85
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-05 12:36:12 +0300

    feat(application): add CreateRoomCommand for creating rooms via MediatR.

---
+++ TravelEase.Application/RoomManagement/Commands/CreateRoomCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using TravelEase.Application.RoomManagement.DTOs.Responses;
+
+namespace TravelEase.Application.RoomManagement.Commands
+{
+    public record CreateRoomCommand : IRequest<RoomResponse?>
+    {
+        public Guid HotelId { get; set; }
+        public Guid RoomTypeId { get; set; }
+        public int AdultsCapacity { get; set; }
+        public int ChildrenCapacity { get; set; }
+        public string View { get; set; }
+        public float Rating { get; set; }
+    }
+}
