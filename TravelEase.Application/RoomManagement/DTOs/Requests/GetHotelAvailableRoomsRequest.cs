16f973df6af25243c7fa3ad84ed96c507ecf6279
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-06 12:54:24 +0300

    feat(api): add GetHotelAvailableRoomsRequest DTO for querying hotel room availability dates.

---
+++ TravelEase.Application/RoomManagement/DTOs/Requests/GetHotelAvailableRoomsRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace TravelEase.Application.RoomManagement.DTOs.Requests
+{
+    public record GetHotelAvailableRoomsRequest
+    {
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+    }
+}
