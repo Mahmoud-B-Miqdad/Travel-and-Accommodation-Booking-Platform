9c0777ea495855b58b54b7200b17e925262f3293
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-09 10:56:12 +0300

    feat(booking/application): validate hotel ownership in DeleteBookingCommandHandler
    - Added hotel existence check using IUnitOfWork.Hotels.
    - Verified booking belongs to specified hotel using IHotelOwnershipValidator.
    - Ensured only authorized users can delete future bookings.

--- TravelEase.Application/BookingManagement/Handlers/DeleteBookingCommandHandler.cs
+++ TravelEase.Application/BookingManagement/Handlers/DeleteBookingCommandHandler.cs
@@ -1,6 +1,5 @@
 ﻿using MediatR;
 using TravelEase.Application.BookingManagement.Commands;
-using TravelEase.Domain.Aggregates.Bookings;
 using TravelEase.Domain.Common.Interfaces;
 using TravelEase.Domain.Exceptions;
 
@@ -9,19 +8,33 @@ namespace TravelEase.Application.BookingManagement.Handlers
     public class DeleteBookingCommandHandler : IRequestHandler<DeleteBookingCommand>
     {
         private readonly IUnitOfWork _unitOfWork;
-        public DeleteBookingCommandHandler(IUnitOfWork unitOfWork)
+        private readonly IHotelOwnershipValidator _hotelOwnershipValidator;
+
+        public DeleteBookingCommandHandler(IUnitOfWork unitOfWork, IHotelOwnershipValidator hotelOwnershipValidator)
         {
             _unitOfWork = unitOfWork;
+            _hotelOwnershipValidator = hotelOwnershipValidator;
         }
 
         public async Task Handle(DeleteBookingCommand request, CancellationToken cancellationToken)
         {
-            var existingBooking = await _unitOfWork.Bookings.GetByIdAsync(request.Id);
+            var hotelExists = await _unitOfWork.Hotels.ExistsAsync(request.HotelId);
+            if (!hotelExists)
+                throw new NotFoundException("Hotel doesn't exists.");
+
+            var existingBooking = await _unitOfWork.Bookings.GetByIdAsync(request.BookingId);
             if (existingBooking == null)
                 throw new NotFoundException("Booking Doesn't Exists To Delete");
 
+            var belongsToHotel = await _hotelOwnershipValidator
+                .IsBookingBelongsToHotelAsync(request.BookingId, request.HotelId);
+
+            if (!belongsToHotel)
+                throw new NotFoundException($"Booking with ID {request.BookingId} does not belong to hotel {request.HotelId}.");
+
+
             var isAccessible = await _unitOfWork.Bookings.IsBookingAccessibleToUserAsync(
-            request.Id, request.GuestEmail);
+            request.BookingId, request.GuestEmail);
 
             if (!isAccessible)
                 throw new UnauthorizedAccessException("You are not authorized to cancel this booking");
