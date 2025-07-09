3c2a0b99cb0483f8b43f9b9ee2b0f127bd744784
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-09 10:46:23 +0300

    refactor(application): update DeleteBookingCommand to include hotelId for ownership validation.

--- TravelEase.Application/BookingManagement/Commands/DeleteBookingCommand.cs
+++ TravelEase.Application/BookingManagement/Commands/DeleteBookingCommand.cs
@@ -4,7 +4,8 @@ namespace TravelEase.Application.BookingManagement.Commands
 {
     public record DeleteBookingCommand : IRequest
     {
-        public Guid Id { get; set; }
+        public Guid HotelId { get; init; }
+        public Guid BookingId { get; init; }
         public string GuestEmail { get; init; }
     }
 }
