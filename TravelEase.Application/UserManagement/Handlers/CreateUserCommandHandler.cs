b1accbeb86314f2b71b26a88087e881e0dc73591
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-09 13:39:58 +0300

    refactor(application): apply SRP to CreateUserCommandHandler for cleaner logic.

--- TravelEase.Application/UserManagement/Handlers/CreateUserCommandHandler.cs
+++ TravelEase.Application/UserManagement/Handlers/CreateUserCommandHandler.cs
@@ -13,32 +13,47 @@ namespace TravelEase.Application.UserManagement.Handlers
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
         private readonly IPasswordHasher _passwordHasher;
-        public CreateUserCommandHandler(IUnitOfWork unitOfWork, IMapper mapper, IPasswordHasher passwordHasher)
+
+        public CreateUserCommandHandler(
+            IUnitOfWork unitOfWork,
+            IMapper mapper,
+            IPasswordHasher passwordHasher)
         {
             _unitOfWork = unitOfWork;
-            _passwordHasher = passwordHasher;
             _mapper = mapper;
+            _passwordHasher = passwordHasher;
         }
 
         public async Task Handle(CreateUserCommand request, CancellationToken cancellationToken)
         {
-            var existingEmail = await _unitOfWork.Hotels.ExistsAsync(request.Email);
-            if (existingEmail)
-                throw new ConflictException($"User with Email '{request.Email}' already exists.");
+            await EnsureEmailIsUniqueAsync(request.Email);
 
             var user = _mapper.Map<User>(request);
-            var uniqueUserSalt = _passwordHasher.GenerateSalt();
-            var userPasswordHash = _passwordHasher.GenerateHashedPassword(request.Password, uniqueUserSalt);
-            if (userPasswordHash is null)
-                throw new InvalidOperationException("Can't Hash User Password");
-
-            user.Id = Guid.NewGuid();
-            user.PasswordHash = userPasswordHash;
-            user.Salt = Convert.ToBase64String(uniqueUserSalt);
-            user.Role = UserRole.Guest;
+            PrepareUser(user, request.Password);
 
             await _unitOfWork.Users.AddAsync(user);
             await _unitOfWork.SaveChangesAsync(cancellationToken);
+        }
+
+        private async Task EnsureEmailIsUniqueAsync(string email)
+        {
+            var exists = await _unitOfWork.Users.ExistsAsync(email);
+            if (exists)
+                throw new ConflictException($"User with email '{email}' already exists.");
+        }
+
+        private void PrepareUser(User user, string password)
+        {
+            var saltBytes = _passwordHasher.GenerateSalt();
+            var hashedPassword = _passwordHasher.GenerateHashedPassword(password, saltBytes);
+
+            if (hashedPassword is null)
+                throw new InvalidOperationException("Can't hash user password.");
+
+            user.Id = Guid.NewGuid();
+            user.Salt = Convert.ToBase64String(saltBytes);
+            user.PasswordHash = hashedPassword;
+            user.Role = UserRole.Guest;
         }
     }
 }
