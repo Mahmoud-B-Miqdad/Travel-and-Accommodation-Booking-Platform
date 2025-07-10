5dcc50a7203acbe847e4f08d862886d6ad60b333
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-10 12:23:57 +0300

    feat(application): add GetAllDiscountsByRoomTypeQuery for fetching paginated discounts by room type.

---
+++ TravelEase.Application/DiscountManagement/Queries/GetAllDiscountsByRoomTypeQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using TravelEase.Application.DiscountManagement.Responses;
+using TravelEase.Domain.Common.Models.PaginationModels;
+
+namespace TravelEase.Application.DiscountManagement.Queries
+{
+    public record GetAllDiscountsByRoomTypeQuery : IRequest<PaginatedList<DiscountResponse>>
+    {
+        public Guid RoomTypeId { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
