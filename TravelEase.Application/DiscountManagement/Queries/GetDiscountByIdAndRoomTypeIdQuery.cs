ae8aa549386adca482ebb39f78dd628cf3e21619
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-10 14:12:20 +0300

    feat(application): add GetDiscountByIdAndRoomTypeIdQuery for retrieving a specific discount.

---
+++ TravelEase.Application/DiscountManagement/Queries/GetDiscountByIdAndRoomTypeIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using TravelEase.Application.DiscountManagement.DTOs.Responses;
+
+namespace TravelEase.Application.DiscountManagement.Queries
+{
+    public record GetDiscountByIdAndRoomTypeIdQuery : IRequest<DiscountResponse>
+    {
+        public Guid RoomTypeId { get; set; }
+        public Guid DiscountId { get; set; }
+    }
+}
