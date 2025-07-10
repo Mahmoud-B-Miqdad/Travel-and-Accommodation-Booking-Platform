b2d3fdeb612667ce159443a4f97aa4321dae3835
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-10 12:42:35 +0300

    feat(application): add DiscountQueryRequest DTO for pagination support.

---
+++ TravelEase.Application/DiscountManagement/DTOs/Requests/DiscountQueryRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace TravelEase.Application.DiscountManagement.DTOs.Requests
+{
+    public record DiscountQueryRequest
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
