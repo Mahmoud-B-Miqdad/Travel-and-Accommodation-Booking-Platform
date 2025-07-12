c7d0881b8c0bdd69be9ad4fc3b5797e96db35c43
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-12 12:36:03 +0300

    feat(application): add RoomTypeResponse DTO for returning room type details.

---
+++ TravelEase.Application/RoomTypeManagement/DTOs/Responses/RoomTypeResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelEase.Application.RoomTypeManagement.DTOs.Responses
+{
+    public record RoomTypeResponse
+    {
+        public Guid Id { get; set; }
+        public Guid HotelId { get; set; }
+        public string Category { get; set; }
+        public float PricePerNight { get; set; }
+    }
+}
