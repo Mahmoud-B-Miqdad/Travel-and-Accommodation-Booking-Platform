d351b722a53de5abb6b8ccd860c8f363779dc5f3
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-12 11:47:27 +0300

    refactor(api): enhance DiscountForCreationRequestValidator with date validity and range checks.

--- TravelEase.API/Validators/DiscountValidators/CreateDiscountRequestValidator.cs
+++ TravelEase.API/Validators/DiscountValidators/CreateDiscountRequestValidator.cs
@@ -7,18 +7,23 @@ namespace TravelEase.API.Validators.DiscountValidators
     {
         public DiscountForCreationRequestValidator()
         {
-            RuleFor(discount => discount.DiscountPercentage)
+            RuleFor(x => x.DiscountPercentage)
                 .GreaterThan(0)
                 .LessThanOrEqualTo(100)
                 .WithMessage("Discount percentage must be between 0 and 100.");
 
-            RuleFor(discount => discount.FromDate)
-                .GreaterThanOrEqualTo(discount => discount.FromDate.AddDays(1))
-                .WithMessage("Check-out date must be at least one day after the check-in date");
+            RuleFor(x => x.FromDate)
+                .Must(BeValidDate).WithMessage("FromDate must be a valid date.")
+                .LessThan(x => x.ToDate).WithMessage("FromDate must be earlier than ToDate.");
 
-            RuleFor(discount => discount.ToDate)
-                .GreaterThanOrEqualTo(discount => discount.ToDate.AddDays(1))
-                .WithMessage("Check-out date must be at least one day after the check-in date");
+            RuleFor(x => x.ToDate)
+                .Must(BeValidDate).WithMessage("ToDate must be a valid date.")
+                .GreaterThan(x => x.FromDate).WithMessage("ToDate must be later than FromDate.");
+        }
+
+        private bool BeValidDate(DateTime date)
+        {
+            return date != default;
         }
     }
 }
