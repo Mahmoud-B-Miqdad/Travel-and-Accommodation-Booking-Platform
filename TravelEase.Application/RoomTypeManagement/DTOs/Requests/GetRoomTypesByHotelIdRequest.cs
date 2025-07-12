7b63522ac5c0bd45066964b7a71acfc9d96da55c
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-12 12:49:55 +0300

    feat(application): add GetRoomTyByHotelIdRequest DTO with pagination and amenities option.

---
+++ TravelEase.Application/RoomTypeManagement/DTOs/Requests/GetRoomTypesByHotelIdRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelEase.Application.RoomTypeManagement.DTOs.Requests
+{
+    public record GetRoomTypesByHotelIdRequest
+    {
+        public bool IncludeAmenities { get; set; } = false;
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
