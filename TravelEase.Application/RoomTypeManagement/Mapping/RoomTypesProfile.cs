a44740aecdbc9304265c5b957e3092a4822dc086
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-12 13:35:58 +0300

    feat(application): add RoomTypesProfile for room type mappings.

---
+++ TravelEase.Application/RoomTypeManagement/Mapping/RoomTypesProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using TravelEase.Application.RoomTypeManagement.DTOs.Requests;
+using TravelEase.Application.RoomTypeManagement.DTOs.Responses;
+using TravelEase.Application.RoomTypeManagement.Queries;
+using TravelEase.Domain.Aggregates.RoomTypes;
+
+namespace TravelEase.Application.ReviewsManagement.Mapping
+{
+    public class RoomTypesProfile : Profile
+    {
+        public RoomTypesProfile()
+        {
+            CreateMap<RoomType, RoomTypeResponse>();
+            CreateMap<GetRoomTypesByHotelIdRequest, GetAllRoomTypesByHotelIdQuery>();
+            CreateMap<RoomTypeResponse, RoomTypeWithoutAmenitiesResponse>();
+        }
+    }
+}
