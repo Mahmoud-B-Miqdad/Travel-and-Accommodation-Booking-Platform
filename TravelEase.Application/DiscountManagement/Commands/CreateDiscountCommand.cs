c26a1f96b3b2efcdd0d7fc8cbc6c43023857c16b
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-12 10:26:06 +0300

    feat(application): add CreateDiscountCommand for adding new discounts.

---
+++ TravelEase.Application/DiscountManagement/Commands/CreateDiscountCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using TravelEase.Application.DiscountManagement.DTOs.Responses;
+
+namespace TravelEase.Application.DiscountManagement.Commands
+{
+    public record CreateDiscountCommand : IRequest<DiscountResponse?>
+    {
+        public Guid RoomTypeId { get; set; }
+        public float DiscountPercentage { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+}
