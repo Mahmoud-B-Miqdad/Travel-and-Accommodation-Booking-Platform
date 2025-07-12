bee0e64a9aba52cacb185bb7f6a302eecade9ff6
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-12 10:24:47 +0300

    feat(application): add DiscountForCreationRequest DTO for creating discounts.

---
+++ TravelEase.Application/DiscountManagement/DTOs/Requests/DiscountForCreationRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelEase.Application.DiscountManagement.DTOs.Requests
+{
+    public record DiscountForCreationRequest
+    {
+        public float DiscountPercentage { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+}
