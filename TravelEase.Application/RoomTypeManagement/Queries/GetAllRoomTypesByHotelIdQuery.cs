9db37f1d369b73f6da890e8cac2a4606900fecf9
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-12 12:38:51 +0300

    feat(application): add GetAllRoomTypesByHotelIdQuery with pagination support.

---
+++ TravelEase.Application/RoomTypeManagement/Queries/GetAllRoomTypesByHotelIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using TravelEase.Application.RoomTypeManagement.DTOs.Responses;
+using TravelEase.Domain.Common.Models.PaginationModels;
+
+namespace TravelEase.Application.RoomTypeManagement.Queries
+{
+    public record GetAllRoomTypesByHotelIdQuery : IRequest<PaginatedList<RoomTypeResponse>>
+    {
+        public Guid HotelId { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
