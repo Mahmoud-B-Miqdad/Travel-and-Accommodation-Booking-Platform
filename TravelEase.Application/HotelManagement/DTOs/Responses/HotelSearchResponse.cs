0486643dfed46b5655f0b51b9bb7c03831caea71
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-14 12:15:23 +0300

    feat(application): add HotelSearchResponse DTO for hotel search result formatting.

---
+++ TravelEase.Application/HotelManagement/DTOs/Responses/HotelSearchResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace TravelEase.Application.HotelManagement.DTOs.Responses
+{
+    public record HotelSearchResponse
+    {
+        public Guid CityId { get; set; }
+        public string CityName { get; set; }
+        public Guid HotelId { get; set; }
+        public Guid RoomId { get; set; }
+        public float RoomPricePerNight { get; set; }
+        public string RoomType { get; set; }
+        public string HotelName { get; set; }
+        public string StreetAddress { get; set; }
+        public int FloorsNumber { get; set; }
+        public float Rating { get; set; }
+        public float Discount { get; set; }
+    }
+}
