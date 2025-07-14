8019b114669518ad7d9be7a83044aeb862c11e1f
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-14 12:05:14 +0300

    feat(application): add HotelSearchRequest DTO for hotel filtering and pagination.

---
+++ TravelEase.Application/HotelManagement/DTOs/Requests/HotelSearchRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelEase.Application.HotelManagement.DTOs.Requests
+{
+    public record HotelSearchRequest
+    {
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public string? CityName { get; set; }
+        public float StarRate { get; set; }
+        public int Adults { get; set; }
+        public int Children { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
