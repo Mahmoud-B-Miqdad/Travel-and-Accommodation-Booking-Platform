550df8a41eed4b3bc115ca147768d3267a04b340
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-14 14:53:40 +0300

    feat(application): add HotelSearchQuery to handle hotel search with filtering and pagination.

---
+++ TravelEase.Application/HotelManagement/Queries/HotelSearchQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using TravelEase.Domain.Common.Models.HotelModels;
+using TravelEase.Domain.Common.Models.PaginationModels;
+
+namespace TravelEase.Application.HotelManagement.Queries
+{
+    public record HotelSearchQuery : IRequest<PaginatedList<HotelSearchResult>>
+    {
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public string? CityName { get; set; }
+        public float StarRate { get; set; } = 3;
+        public int Adults { get; set; } = 2;
+        public int Children { get; set; } = 1;
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 5;
+    }
+}
