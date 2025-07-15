ccb2685494d8d26ecbcafebe0bd03a2acca10037
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-15 14:39:39 +0300

    feat(application): add query for recently visited hotels by authenticated guest email.

---
+++ TravelEase.Application/HotelManagement/Queries/GetRecentlyVisitedHotelsForAuthenticatedGuestQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using TravelEase.Application.HotelManagement.DTOs.Responses;
+
+namespace TravelEase.Application.HotelManagement.Queries
+{
+    public record GetRecentlyVisitedHotelsForAuthenticatedGuestQuery : IRequest<List<HotelWithoutRoomsResponse>>
+    {
+        public string GuestEmail { get; set; }
+        public int Count { get; set; } = 5;
+    }
+}
