c20eb01058d1248e28b19f0ef54cc0c3e0910fba
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-15 12:45:43 +0300

    feat(application): add GetFeaturedDealsQuery to retrieve top featured hotel deals.

---
+++ TravelEase.Application/HotelManagement/Queries/GetFeaturedDealsQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TravelEase.Domain.Common.Models.CommonModels;
+
+namespace TravelEase.Application.HotelManagement.Queries
+{
+    public record GetFeaturedDealsQuery : IRequest<List<FeaturedDeal>>
+    {
+        public int Count { get; set; } = 5;
+    }
+}
