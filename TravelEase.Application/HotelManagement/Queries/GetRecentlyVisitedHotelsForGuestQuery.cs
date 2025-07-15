4c4f7f69f2f35e1b289f866fee931789c32ee2fd
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-15 13:59:22 +0300

    feat(application): add GetRecentlyVisitedHotelsForGuestQuery to fetch recent guest visits.

---
+++ TravelEase.Application/HotelManagement/Queries/GetRecentlyVisitedHotelsForGuestQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using TravelEase.Application.HotelManagement.DTOs.Responses;
+
+namespace TravelEase.Application.HotelManagement.Queries
+{
+    public record GetRecentlyVisitedHotelsForGuestQuery : IRequest<List<HotelWithoutRoomsResponse>>
+    {
+        public Guid GuestId { get; set; }
+        public int Count { get; set; } = 5;
+    }
+}
