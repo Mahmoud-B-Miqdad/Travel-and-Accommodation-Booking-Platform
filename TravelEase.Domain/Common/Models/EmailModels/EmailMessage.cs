7801a15703b4c23cd9408b2df131994d546d39c1
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-16 13:52:31 +0300

    feat(domain): add EmailMessage model for email service abstraction.

---
+++ TravelEase.Domain/Common/Models/EmailModels/EmailMessage.cs
@@ -0,0 +1,18 @@
+﻿namespace TravelEase.Domain.Common.Models.EmailModels
+{
+    public class EmailMessage
+    {
+        public List<string> To { get; set; } = new();
+        public string Subject { get; set; } = string.Empty;
+        public string PlainTextContent { get; set; } = string.Empty;
+        public string HtmlContent { get; set; } = string.Empty;
+
+        public EmailMessage(IEnumerable<string> to, string subject, string plainText, string html)
+        {
+            To = to.ToList();
+            Subject = subject;
+            PlainTextContent = plainText;
+            HtmlContent = html;
+        }
+    }
+}
