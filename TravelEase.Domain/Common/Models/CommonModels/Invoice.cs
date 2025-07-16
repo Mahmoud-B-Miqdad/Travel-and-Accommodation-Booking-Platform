eefa6941d26f108372d81ec04ff65e44b542acc6
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-16 10:57:33 +0300

    feat(domain): add Invoice record model for representing booking invoice details.

---
+++ TravelEase.Domain/Common/Models/CommonModels/Invoice.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelEase.Domain.Common.Models.CommonModels
+{
+    public record Invoice
+    {
+        public Guid Id { get; set; }
+        public DateTime BookingDate { get; set; }
+        public double Price { get; set; }
+        public string HotelName { get; set; }
+        public string OwnerName { get; set; }
+    }
+}
