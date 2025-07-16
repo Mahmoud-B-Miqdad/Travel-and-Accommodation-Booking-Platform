188bff2f9d77d0147e1de74a2b5c0e5bb368a6e1
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-16 11:15:28 +0300

    feat(application): add GetBookingInvoiceQuery to retrieve booking invoice by ID.

---
+++ TravelEase.Application/BookingManagement/Queries/GetBookingInvoiceQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace TravelEase.Application.BookingManagement.Queries
+{
+    public record GetBookingInvoiceQuery : IRequest<InvoiceResponse>
+    {
+        public Guid BookingId { get; set; }
+    }
+}
