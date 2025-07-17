7123187c4fff942c0beb0d34e621efffdc1f0dc9
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-17 11:25:22 +0300

    feat(application): extend GetInvoiceByBookingIdQuery with email and PDF generation flags.

--- TravelEase.Application/BookingManagement/Queries/GetInvoiceByBookingIdQuery.cs
+++ TravelEase.Application/BookingManagement/Queries/GetInvoiceByBookingIdQuery.cs
@@ -7,5 +7,8 @@ namespace TravelEase.Application.BookingManagement.Queries
         public Guid HotelId { get; set; }
         public Guid BookingId { get; set; }
         public string GuestEmail { get; set; }
+        public string GuestName { get; set; }
+        public bool GeneratePdf { get; set; } = false;
+        public bool SendByEmail { get; set; } = false;
     }
 }
