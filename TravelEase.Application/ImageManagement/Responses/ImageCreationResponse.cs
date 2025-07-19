cd1d42f3ad8b2e4a4396eaf57607df51800fc225
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-19 11:31:20 +0300

    feat(application): add ImageCreationResponse record with image metadata.

---
+++ TravelEase.Application/ImageManagement/Responses/ImageCreationResponse.cs
@@ -0,0 +1,12 @@
+﻿using TravelEase.Domain.Enums;
+
+namespace TravelEase.Application.ImageManagement.Responses
+{
+    public record ImageCreationResponse
+    {
+        public Guid EntityId { get; set; }
+        public string Base64Content { get; set; }
+        public ImageFormat Format { get; set; }
+        public ImageType Type { get; set; }
+    }
+}
