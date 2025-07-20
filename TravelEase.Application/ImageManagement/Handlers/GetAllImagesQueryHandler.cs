28884b1598fc6a241c82b91ed3699c4230d0798e
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-20 12:29:13 +0300

    feat(application): implement GetAllImagesQueryHandler with pagination and entity existence check.

---
+++ TravelEase.Application/ImageManagement/Handlers/GetAllImagesQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using TravelEase.Application.ImageManagement.Queries;
+using TravelEase.Domain.Aggregates.Hotels;
+using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Domain.Common.Models.PaginationModels;
+using TravelEase.Domain.Exceptions;
+
+namespace TravelEase.Application.ImageManagement.Handlers
+{
+    public class GetAllImagesQueryHandler : IRequestHandler<GetAllImagesQuery, PaginatedList<string>>
+    {
+        private readonly IImageService _imageService;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetAllImagesQueryHandler(IImageService imageService, IUnitOfWork unitOfWork)
+        {
+            _imageService = imageService;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<PaginatedList<string>> Handle
+            (GetAllImagesQuery request, CancellationToken cancellationToken)
+        {
+            var hotelExists = await _unitOfWork.Hotels.ExistsAsync(request.HotelId);
+
+            if (!hotelExists)
+                throw new NotFoundException($"Hotel with ID {request.HotelId} was not found.");
+
+            var imageUrls = await _imageService.GetAllImagesAsync
+                (request.HotelId, request.PageNumber, request.PageSize);
+
+            return imageUrls;
+        }
+    }
+}
