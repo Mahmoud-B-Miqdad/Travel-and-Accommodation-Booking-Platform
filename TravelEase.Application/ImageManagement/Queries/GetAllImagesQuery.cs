95031f399f37d3ffdae190a94182290cd111bb8e
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-20 12:08:49 +0300

    feat(application): add GetAllImagesQuery with pagination properties.

---
+++ TravelEase.Application/ImageManagement/Queries/GetAllImagesQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace TravelEase.Application.ImageManagement.Queries
+{
+    public record GetAllImagesQuery(Guid EntityId) : IRequest<List<string>>
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
