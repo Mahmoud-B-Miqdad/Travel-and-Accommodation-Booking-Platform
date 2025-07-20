03e160e084e97fcd63898975025449f5bf9988d2
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-20 12:11:41 +0300

    feat(api): add FluentValidation for GetAllImagesQuery.

---
+++ TravelEase.API/Validators/ImageValidators/GetAllImagesQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using TravelEase.Application.ImageManagement.Queries;
+
+namespace TravelEase.API.Validators.ImageValidators
+{
+    public class GetAllImagesQueryValidator : AbstractValidator<GetAllImagesQuery>
+    {
+        public GetAllImagesQueryValidator()
+        {
+            RuleFor(x => x.PageNumber)
+                .GreaterThan(0).WithMessage("Page number must be greater than 0.");
+
+            RuleFor(x => x.PageSize)
+                .GreaterThan(0).WithMessage("Page size must be greater than 0.");
+        }
+    }
+}
