6fa95569ab10c55bf2b5f25c1991a4b7780fd9db
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-20 14:39:38 +0300

    feat(application): implement UploadHotelImageCommandHandler with hotel existence check and image upload logic.

---
+++ TravelEase.Application/ImageManagement/Handlers/UploadHotelImageCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using TravelEase.Application.ImageManagement.Commands;
+using TravelEase.Application.ImageManagement.Mappings;
+using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Domain.Enums;
+using TravelEase.Domain.Exceptions;
+
+namespace TravelEase.Application.ImageManagement.Handlers
+{
+    public class UploadHotelImageCommandHandler : IRequestHandler<UploadHotelImageCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IImageService _imageService;
+
+        public UploadHotelImageCommandHandler(IUnitOfWork unitOfWork, IImageService imageService)
+        {
+            _unitOfWork = unitOfWork;
+            _imageService = imageService;
+        }
+
+        public async Task Handle(UploadHotelImageCommand request, CancellationToken cancellationToken)
+        {
+            await EnsureHotelExists(request.HotelId, cancellationToken);
+
+            var imageCreationDto = await ImageFormFileMapper.FromFormFileAsync(
+                request.HotelId, request.File, ImageType.Gallery);
+
+            await _imageService.UploadImageAsync(imageCreationDto);
+        }
+
+        private async Task EnsureHotelExists(Guid hotelId, CancellationToken cancellationToken)
+        {
+            if (!await _unitOfWork.Hotels.ExistsAsync(hotelId))
+                throw new NotFoundException("Hotel doesn't exist.");
+        }
+    }
+}
