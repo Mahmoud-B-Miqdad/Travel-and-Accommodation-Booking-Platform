93c74ec5d0296c2afc2459349d28d7ff447e383f
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-22 12:20:59 +0300

    feat(application): implement DeleteImageCommandHandler to delete hotel images.

---
+++ TravelEase.Application/ImageManagement/Handlers/DeleteImageCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using TravelEase.Application.ImageManagement.Commands;
+using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Domain.Exceptions;
+
+namespace TravelEase.Application.ImageManagement.Handlers
+{
+    internal class DeleteImageCommandHandler : IRequestHandler<DeleteImageCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IImageService _imageService;
+
+        public DeleteImageCommandHandler(IUnitOfWork unitOfWork, IImageService imageService)
+        {
+            _unitOfWork = unitOfWork;
+            _imageService = imageService;
+        }
+
+        public async Task Handle(DeleteImageCommand request, CancellationToken cancellationToken)
+        {
+            await EnsureHotelExists(request.HotelId);
+
+            await _imageService.DeleteImageAsync(request.HotelId, request.ImageId);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+        }
+
+        private async Task EnsureHotelExists(Guid hotelId)
+        {
+            if (!await _unitOfWork.Hotels.ExistsAsync(hotelId))
+                throw new NotFoundException("Hotel doesn't exist.");
+        }
+    }
+}
