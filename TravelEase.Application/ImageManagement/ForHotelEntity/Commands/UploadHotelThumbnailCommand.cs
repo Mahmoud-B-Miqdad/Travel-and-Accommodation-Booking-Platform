f1c73bad6c7cbe3e7e4cff87fa17b4d5105ec9df
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-22 13:28:27 +0300

    feat(application): add UploadCityThumbnailCommand to handle thumbnail upload requests for cities.

--- TravelEase.Application/ImageManagement/ForHotelEntity/Commands/UploadHotelThumbnailCommand.cs
+++ TravelEase.Application/ImageManagement/ForHotelEntity/Commands/UploadHotelThumbnailCommand.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Http;
 
 namespace TravelEase.Application.ImageManagement.ForHotelEntity.Commands
 {
-    public class UploadHotelThumbnailCommand : IRequest
+    public record UploadHotelThumbnailCommand : IRequest
     {
         public Guid HotelId { get; init; }
         public IFormFile File { get; init; }
