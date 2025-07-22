29b653ee16a9a8f81363e1ab8c9ee8da062daf9e
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-22 12:13:42 +0300

    feat(application): add DeleteImageCommand for hotel image deletion via CQRS.

---
+++ TravelEase.Application/ImageManagement/Commands/DeleteImageCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace TravelEase.Application.ImageManagement.Commands
+{
+    public class DeleteImageCommand : IRequest
+    {
+        public Guid HotelId { get; set; }
+        public Guid ImageId { get; set; }
+    }
+}
