befc4d384807a8d09ac569b3d73f0c48b92d1e8e
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-23 14:04:11 +0300

    feat(application): add CreatePaymentIntentCommand with booking and payment method details.

---
+++ TravelEase.Application/PaymentManagement/Commands/CreatePaymentIntentCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using TravelEase.Domain.Enums;
+
+namespace TravelEase.Application.PaymentManagement.Commands
+{
+    public record CreatePaymentIntentCommand : IRequest<string>
+    {
+        public Guid BookingId { get; set; }
+        public string Amount { get; set; }
+        public string GuestEmail { get; set; }
+        public PaymentMethod Method { get; set; }
+    }
+}
