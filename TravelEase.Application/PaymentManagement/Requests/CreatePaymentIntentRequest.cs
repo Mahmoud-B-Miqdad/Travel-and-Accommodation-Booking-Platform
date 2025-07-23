0811a161b51037e03f127e581354e7400eaf094a
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-23 14:26:32 +0300

    feat(application): add CreatePaymentIntentRequest DTO for initiating Stripe payments.

---
+++ TravelEase.Application/PaymentManagement/Requests/CreatePaymentIntentRequest.cs
@@ -0,0 +1,11 @@
+﻿using TravelEase.Domain.Enums;
+
+namespace TravelEase.Application.PaymentManagement.Requests
+{
+    public record CreatePaymentIntentRequest
+    {
+        public Guid BookingId { get; set; }
+        public double Amount { get; set; }
+        public PaymentMethod Method { get; set; }
+    }
+}
