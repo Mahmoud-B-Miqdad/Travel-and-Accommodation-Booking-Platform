db6995e84a94704bbbcee4fa56f5c118afdac14d
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-23 10:28:51 +0300

    feat(domain): add Payment entity and establish one-to-one relation with Booking.

--- TravelEase.Domain/Aggregates/Bookings/Booking.cs
+++ TravelEase.Domain/Aggregates/Bookings/Booking.cs
@@ -1,4 +1,5 @@
-﻿using TravelEase.Domain.Aggregates.Reviews;
+﻿using TravelEase.Domain.Aggregates.Payments;
+using TravelEase.Domain.Aggregates.Reviews;
 using TravelEase.Domain.Aggregates.Rooms;
 using TravelEase.Domain.Aggregates.Users;
 
@@ -12,6 +13,7 @@ namespace TravelEase.Domain.Aggregates.Bookings
         public Guid UserId { get; set; }
         public User User { get; set; }
         public Review? Review { get; set; }
+        public Payment? Payment { get; set; }
         public DateTime CheckInDate { get; set; }
         public DateTime CheckOutDate { get; set; }
         public DateTime BookingDate { get; set; }
