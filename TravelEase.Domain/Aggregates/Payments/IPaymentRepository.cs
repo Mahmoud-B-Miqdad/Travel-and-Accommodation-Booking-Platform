c36bfc1e4da32df296af75b099711ca6db605450
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-23 11:25:07 +0300

    feat(infra/domain): add PaymentRepository and IPaymentRepository with user-specific query method.

---
+++ TravelEase.Domain/Aggregates/Payments/IPaymentRepository.cs
@@ -0,0 +1,9 @@
+﻿using TravelEase.Domain.Common.Interfaces;
+
+namespace TravelEase.Domain.Aggregates.Payments
+{
+    public interface IPaymentRepository : ICrudRepository<Payment>
+    {
+        Task<List<Payment>> GetPaymentsByUserIdAsync(Guid userId);
+    }
+}
---
+++ TravelEase.Infrastructure/Persistence/EntityPersistence/PaymentPersistence/PaymentRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelEase.Domain.Aggregates.Payments;
+using TravelEase.Infrastructure.Persistence.CommonRepositories;
+using TravelEase.Infrastructure.Persistence.Context;
+
+namespace TravelEase.Infrastructure.Persistence.EntityPersistence.PaymentPersistence
+{
+    public class PaymentRepository : GenericCrudRepository<Payment>, IPaymentRepository
+    {
+        private readonly TravelEaseDbContext _context;
+
+        public PaymentRepository(TravelEaseDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Payment>> GetPaymentsByUserIdAsync(Guid userId)
+        {
+            return await _context.Payments
+                .Where(p => p.Booking.UserId == userId)
+                .ToListAsync();
+        }
+    }
+}
