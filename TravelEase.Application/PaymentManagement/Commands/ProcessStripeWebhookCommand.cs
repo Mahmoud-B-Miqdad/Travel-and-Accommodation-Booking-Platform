eb5355aa79636ef5e65849652ca400226b6f2434
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-07-24 11:08:14 +0300

    feat(application): add ProcessStripeWebhookCommand to handle incoming Stripe events.

---
+++ TravelEase.Application/PaymentManagement/Commands/ProcessStripeWebhookCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Stripe;
+
+namespace TravelEase.Application.PaymentManagement.Commands
+{
+    public record ProcessStripeWebhookCommand : IRequest
+    {
+        public Event StripeEvent { get; set; }
+    }
+}
