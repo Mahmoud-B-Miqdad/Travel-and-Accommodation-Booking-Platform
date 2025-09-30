f8b9e6bb3238b12697ced902d211dca97b3d41b9
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-09-30 11:45:10 +0300

    chore(docker+infra): update Dockerfile for DinkToPdf dependencies and register IConverter
    - Installed required Linux libraries for DinkToPdf in Dockerfile
    - Switched user to app after installation
    - Registered PdfConverterFactory.CreateConverter() as a singleton IConverter in Program.cs.

--- backend/TravelEase.Infrastructure/Common/Extensions/InfrastructureServiceExtensions.cs
+++ backend/TravelEase.Infrastructure/Common/Extensions/InfrastructureServiceExtensions.cs
@@ -1,38 +1,38 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DinkToPdf.Contracts;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TravelEase.Domain.Aggregates.Bookings;
 using TravelEase.Domain.Aggregates.Cities;
 using TravelEase.Domain.Aggregates.Discounts;
 using TravelEase.Domain.Aggregates.Hotels;
+using TravelEase.Domain.Aggregates.Images;
+using TravelEase.Domain.Aggregates.Payments;
 using TravelEase.Domain.Aggregates.Reviews;
 using TravelEase.Domain.Aggregates.RoomAmenities;
 using TravelEase.Domain.Aggregates.Rooms;
 using TravelEase.Domain.Aggregates.RoomTypes;
 using TravelEase.Domain.Aggregates.Users;
 using TravelEase.Domain.Common.Interfaces;
+using TravelEase.Infrastructure.Common.Security;
+using TravelEase.Infrastructure.Persistence.CommonRepositories;
 using TravelEase.Infrastructure.Persistence.Context;
 using TravelEase.Infrastructure.Persistence.EntityPersistence.BookingPersistence;
 using TravelEase.Infrastructure.Persistence.EntityPersistence.CityPersistence;
 using TravelEase.Infrastructure.Persistence.EntityPersistence.DiscountPersistence;
 using TravelEase.Infrastructure.Persistence.EntityPersistence.HotelPersistence;
+using TravelEase.Infrastructure.Persistence.EntityPersistence.ImagePersistence;
+using TravelEase.Infrastructure.Persistence.EntityPersistence.PaymentPersistence;
 using TravelEase.Infrastructure.Persistence.EntityPersistence.ReviewPersistence;
 using TravelEase.Infrastructure.Persistence.EntityPersistence.RoomAmenityPersistence;
 using TravelEase.Infrastructure.Persistence.EntityPersistence.RoomPersistence;
 using TravelEase.Infrastructure.Persistence.EntityPersistence.RoomTypePersistence;
 using TravelEase.Infrastructure.Persistence.EntityPersistence.UserPersistence;
-using TravelEase.Infrastructure.Persistence.CommonRepositories;
-using TravelEase.Infrastructure.Common.Security;
 using TravelEase.Infrastructure.Persistence.Services.CommonServices;
-using TravelEase.Infrastructure.Persistence.Services.PDFServices;
-using NReco.PdfGenerator;
 using TravelEase.Infrastructure.Persistence.Services.EmailService;
 using TravelEase.Infrastructure.Persistence.Services.ImageServices;
-using TravelEase.Domain.Aggregates.Images;
-using TravelEase.Infrastructure.Persistence.EntityPersistence.ImagePersistence;
 using TravelEase.Infrastructure.Persistence.Services.PaymentServices;
-using TravelEase.Domain.Aggregates.Payments;
-using TravelEase.Infrastructure.Persistence.EntityPersistence.PaymentPersistence;
+using TravelEase.Infrastructure.Persistence.Services.PDFServices;
 
 namespace TravelEase.Infrastructure.Common.Extensions
 {
@@ -65,10 +65,10 @@ namespace TravelEase.Infrastructure.Common.Extensions
             services.AddScoped(typeof(IReadableRepository<>), typeof(GenericReadableRepository<>));
 
             services.AddScoped<IInvoiceHtmlGenerator, InvoiceHtmlGenerator>();
+            services.AddSingleton(typeof(IConverter), PdfConverterFactory.CreateConverter());
             services.AddScoped<IPdfService, PdfService>();
             services.AddScoped<IInvoiceEmailBuilder, InvoiceEmailBuilder>();
             services.AddScoped<IEmailService, SendGridEmailService>();
-            services.AddScoped<HtmlToPdfConverter>();
             services.AddScoped<IImageService, CloudinaryImageService>();
             services.AddScoped<IPaymentService, StripePaymentService>();
 
